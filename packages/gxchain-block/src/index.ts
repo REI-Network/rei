e821fb9004470cd70c56e88065edd444b9744433
samlior <samlior@foxmail.com>
2020-12-14 14:03:28 +0800

    feat: add block and blockchain

---
+++ packages/gxchain-block/src/index.ts
@@ -0,0 +1,4 @@
+import { Block as EthereumJSBlock, BlockHeader as EthereumJSBlockHeander } from '@ethereumjs/block';
+
+export class Block extends EthereumJSBlock {}
+export class BlockHeader extends EthereumJSBlockHeander {}
--- packages/gxchain-blockchain/src/index.ts
+++ packages/gxchain-blockchain/src/index.ts
@@ -1,5 +1,41 @@
-import EthereumJSBlockchain, { BlockchainOptions } from '@ethereumjs/blockchain';
+import { EventEmitter } from 'events';
 
-class Blockchain extends EthereumJSBlockchain {}
+import EthereumJSBlockchain from '@ethereumjs/blockchain';
 
-export { Blockchain };
+import { Block, BlockHeader } from '@gxchain2/block';
+
+interface Constructor<T = {}> {
+  new (...args: any[]): T;
+}
+
+declare function mixin<T1 extends Constructor, T2 extends Constructor>(mix1: T1, mix2: T2): new (...args) => InstanceType<T1> & InstanceType<T2>;
+
+export declare interface Blockchain {
+  on(event: 'updated', listener: (block: Block) => void): this;
+}
+
+export class Blockchain extends mixin(EthereumJSBlockchain, EventEmitter) {
+  private _latestBlock!: Block;
+
+  get latestBlock() {
+    return this._latestBlock;
+  }
+
+  private async updateLatest() {
+    const latestBlock = await this.getLatestBlock();
+    if (!this._latestBlock || !latestBlock.header.hash().equals(this._latestBlock.header.hash())) {
+      this._latestBlock = latestBlock;
+      this.emit('updated', this._latestBlock);
+    }
+  }
+
+  async putBlock(block: Block) {
+    await super.putBlock(block);
+    await this.updateLatest();
+  }
+
+  async putHeader(header: BlockHeader) {
+    await super.putHeader(header);
+    await this.updateLatest();
+  }
+}

c2b45cedfb4c9a7f3bc127cbc4a9812fcf2894d7
samlior <samlior@foxmail.com>
2020-12-21 13:47:53 +0800

    chore: add new handler for protocol

--- packages/gxchain-network/src/protocol/ethprotocol.ts
+++ packages/gxchain-network/src/protocol/ethprotocol.ts
@@ -1,6 +1,7 @@
 import { bufferToInt, rlp } from 'ethereumjs-util';
 
 import { constants } from '@gxchain2/common';
+import { BlockHeader, BlockHeaderBuffer } from '@gxchain2/block';
 
 import { Protocol, Handler } from './protocol';
 
@@ -24,6 +25,23 @@ const handlers: Handler[] = [
         genesisHash: status.genesisHash
       };
     }
+  },
+  {
+    name: 'GetBlockHeaders',
+    code: 1,
+    response: 2,
+    encode: (data) => {
+      return rlp.encode([1, data.start, data.count]);
+    },
+    decode: ([start, count]: Buffer[]) => {
+      return { start: bufferToInt(start), count: bufferToInt(count) };
+    }
+  },
+  {
+    name: 'BlockHeaders',
+    code: 2,
+    encode: (headers: BlockHeader[]) => headers.map((h) => h.raw()),
+    decode: (headers: BlockHeaderBuffer[]) => headers.map((h) => BlockHeader.fromValuesArray(h, {}))
   },
   {
     name: 'Echo',

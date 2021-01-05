9db55d0121bde4134e72899c7b2e19ef2aaa752b
samlior <samlior@foxmail.com>
2021-01-05 10:24:31 +0800

    feat: add database impl

--- packages/gxchain-database/src/index.ts
+++ packages/gxchain-database/src/index.ts
@@ -1,18 +1,1 @@
-import levelUp from 'levelup';
-import levelDown from 'leveldown';
-import type { LevelUp } from 'levelup';
-import encoding from 'encoding-down';
-import { DBManager } from '@ethereumjs/blockchain/dist/db/manager';
-export { DBOp, DBSetBlockOrHeader, DBSetTD, DBSetHashToNumber, DBSaveLookups } from '@ethereumjs/blockchain/dist/db/helpers';
-
-import type { Common } from '@gxchain2/common';
-
-export class Database extends DBManager {
-  constructor(db: LevelUp, common: Common) {
-    super(db, common);
-  }
-}
-
-export const createLevelDB = (path: string) => {
-  return levelUp(encoding(levelDown(path)));
-};
+export { DBManager } from './db/manager';

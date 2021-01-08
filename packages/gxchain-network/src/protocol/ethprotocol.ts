aece555116c6997dc7e0c46301e0616f7a890c8b
samlior <samlior@foxmail.com>
2021-01-08 18:10:50 +0800

    chore: add get bodies and handle bodies

--- packages/gxchain-network/src/protocol/ethprotocol.ts
+++ packages/gxchain-network/src/protocol/ethprotocol.ts
@@ -1,7 +1,8 @@
 import { bufferToInt, rlp } from 'ethereumjs-util';
 
 import { constants } from '@gxchain2/common';
-import { BlockHeader, BlockHeaderBuffer } from '@gxchain2/block';
+import { BlockHeader, BlockHeaderBuffer, TransactionsBuffer } from '@gxchain2/block';
+import { Transaction } from '@gxchain2/tx';
 
 import { Protocol, Handler } from './protocol';
 import type { INode } from '../p2p';
@@ -31,13 +32,11 @@ const handlers: Handler[] = [
     name: 'GetBlockHeaders',
     code: 1,
     response: 2,
-    encode: (data) => {
-      return rlp.encode([1, [data.start, data.count]]);
-    },
+    encode: ({ start, count }: { start: number; count: number }) => rlp.encode([1, [start, count]]),
     decode: ([start, count]: Buffer[]) => {
       return { start: bufferToInt(start), count: bufferToInt(count) };
     },
-    async process(node: INode, { start, count }: { start: number; count: number }): Promise<[string, any]> {
+    async process(node: INode, { start, count }: { start: number; count: number }): Promise<[string, BlockHeader[]]> {
       const blocks = await node.blockchain.getBlocks(start, count, 0, false);
       return ['BlockHeaders', blocks.map((b) => b.header)];
     }
@@ -46,7 +45,50 @@ const handlers: Handler[] = [
     name: 'BlockHeaders',
     code: 2,
     encode: (headers: BlockHeader[]) => rlp.encode([2, headers.map((h) => h.raw())]),
-    decode: (headers: BlockHeaderBuffer[]) => headers.map((h) => BlockHeader.fromValuesArray(h, {}))
+    decode: (headers: BlockHeaderBuffer[]) => headers.map((h) => BlockHeader.fromValuesArray(h))
+  },
+  {
+    name: 'GetBlockBodies',
+    code: 3,
+    response: 4,
+    encode: (headers: BlockHeader[]) => rlp.encode([3, headers.map((h) => h.hash())]),
+    decode: (headerHashs: Buffer[]) => headerHashs,
+    /*
+    decode: (bodies: BlockBodyBuffer[]): Transaction[][] =>
+      bodies.map((b) => {
+        const txsBuffer = b[0];
+        return txsBuffer.map((txBuffer) => Transaction.fromValuesArray(txBuffer));
+      }),
+      */
+    async process(node: INode, headerHashs: Buffer[]): Promise<[string, Transaction[][]]> {
+      const bodies: Transaction[][] = [];
+      for (const hash of headerHashs) {
+        try {
+          const block = await node.db.getBlock(hash);
+          bodies.push(block.transactions);
+        } catch (err) {
+          if (err.type !== 'NotFoundError') {
+            throw err;
+          }
+        }
+      }
+      return ['BlockBodies', bodies];
+    }
+  },
+  {
+    name: 'BlockBodies',
+    code: 4,
+    encode: (bodies: Transaction[][]) =>
+      rlp.encode([
+        4,
+        bodies.map((txs) => {
+          return txs.map((tx) => tx.raw());
+        })
+      ]),
+    decode: (bodies: TransactionsBuffer[]): Transaction[][] =>
+      bodies.map((txs) => {
+        return txs.map((tx) => Transaction.fromValuesArray(tx));
+      })
   },
   {
     name: 'Echo',

c9949d5e2a7dbb13b8c7408f1f58c8f9e985f1a1
samlior <samlior@foxmail.com>
2021-01-08 11:45:12 +0800

    chore: add async heap

--- packages/gxchain-utils/src/asyncnext.ts
+++ packages/gxchain-utils/src/asyncnext.ts
@@ -1,3 +1,5 @@
+import Heap from 'qheap';
+
 interface AsyncNextOption<T> {
   push: (data: T | null) => void;
   hasNext: () => boolean;
@@ -48,7 +50,9 @@ export class AsyncQueue<T = any> extends AsyncNext<T> {
       Object.assign(
         {
           push: (data: T | null) => {
-            this.arr.push(data);
+            if (data !== null) {
+              this.arr.push(data);
+            }
           },
           hasNext: () => this.arr.length > 0,
           next: () => this.arr.shift()!
@@ -73,6 +77,51 @@ export class AsyncQueue<T = any> extends AsyncNext<T> {
   }
 }
 
+interface AsyncHeapOption<T> {
+  compare?: (a: T, b: T) => number;
+  push?: (data: T | null) => void;
+  hasNext?: () => boolean;
+  next?: () => T | null;
+}
+
+export class AsyncHeap<T = any> extends AsyncNext<T> {
+  private h: Heap;
+  private compare?: (a: T, b: T) => number;
+
+  constructor(options?: AsyncHeapOption<T>) {
+    super(
+      Object.assign(
+        {
+          push: (data: T | null) => {
+            if (data !== null) {
+              this.h.insert(data);
+            }
+          },
+          hasNext: () => this.h.length > 0,
+          next: () => this.h.remove()
+        },
+        options
+      )
+    );
+    this.compare = options?.compare;
+    this.h = new Heap(this.compare ? { comparBefore: this.compare } : undefined);
+  }
+
+  get heap() {
+    return this.h;
+  }
+
+  clear() {
+    this.h = new Heap(this.compare ? { comparBefore: this.compare } : undefined);
+  }
+
+  abort() {
+    if (this.isWaiting) {
+      this.push(null);
+    }
+  }
+}
+
 interface AysncChannelOption<T> extends AsyncQueueOption<T> {
   isAbort: () => boolean;
 }
@@ -81,6 +130,29 @@ export class AysncChannel<T = any> extends AsyncQueue<T> {
   private isAbort: () => boolean;
 
   constructor(options: AysncChannelOption<T>) {
+    super(options);
+    this.isAbort = options.isAbort;
+  }
+
+  async *generator() {
+    while (!this.isAbort()) {
+      const result = await this.next();
+      if (this.isAbort() || result === null) {
+        return;
+      }
+      yield result;
+    }
+  }
+}
+
+interface AysncHeapChannelOption<T> extends AsyncHeapOption<T> {
+  isAbort: () => boolean;
+}
+
+export class AysncHeapChannel<T = any> extends AsyncHeap<T> {
+  private isAbort: () => boolean;
+
+  constructor(options: AysncHeapChannelOption<T>) {
     super(options);
     this.isAbort = options.isAbort;
   }

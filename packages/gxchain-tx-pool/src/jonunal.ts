a979889689d82c1c82f34170b4567af98d25ba07
sspin <>
2021-02-02 17:08:24 +0800

    chore: fix the override bug

--- packages/gxchain-tx-pool/src/jonunal.ts
+++ packages/gxchain-tx-pool/src/jonunal.ts
@@ -1,188 +1,141 @@
-import * as fs from 'fs';
+import fs from 'fs';
 import { Transaction } from '@gxchain2/tx';
 import { Address, rlp } from 'ethereumjs-util';
 import readline from 'readline';
 import { EventEmitter } from 'events';
-import stream from 'stream';
-import { resolve } from 'path';
-import { rejects } from 'assert';
+
+const bufferSplit = Buffer.from('\r\n');
 
 export class jonunal extends EventEmitter {
   path: string;
-  writer: fs.WriteStream;
+  writer?: fs.WriteStream;
   constructor(path: string) {
     super();
     this.path = path;
-    this.writer = fs.createWriteStream(this.path);
+    this.writer = fs.createWriteStream(this.path, { flags: 'a' });
   }
 
   load(add: (transaction: Transaction) => void) {
     try {
-      fs.existsSync(this.path);
-    } catch (err) {
-      this.emit('error', err);
-    }
-
-    try {
+      if (!fs.existsSync(this.path)) {
+        fs.createWriteStream(this.path, { flags: 'a' });
+      }
       var inputer = fs.createReadStream(this.path);
-    } catch (err) {
-      this.emit('error', err);
-    }
-    var inputer = fs.createReadStream(this.path);
-    this.writer.close;
+      if (this.writer) {
+        this.writer.end();
+        this.writer = undefined;
+      }
 
-    let total = 0;
-    let dropped = 0;
+      let total = 0;
+      let dropped = 0;
 
-    let loadBath = (txs: Transaction[]) => {
-      txs.forEach(function (tx) {
-        try {
-          add(tx);
-        } catch (err) {
-          console.log('Failed to add journaled transaction', 'err', err);
-          dropped++;
-        }
-      });
-    };
+      let loadBath = (txs: Transaction[]) => {
+        txs.forEach(function (tx) {
+          try {
+            add(tx);
+          } catch (err) {
+            console.log('Failed to add journaled transaction', 'err', err);
+            dropped++;
+          }
+        });
+      };
 
-    let batch: Transaction[] = [];
-
-    let bufferSplit = Buffer.from('\r\n');
-    let bufferInput: Buffer = Buffer.from('');
-    inputer.on('data', (chunk: Buffer) => {
-      try {
+      let batch: Transaction[] = [];
+      let bufferInput: Buffer = Buffer.from('');
+      inputer.on('data', (chunk: Buffer) => {
         bufferInput = Buffer.concat([bufferInput, chunk]);
-        let end = bufferInput.length;
         while (true) {
           let i = bufferInput.indexOf(bufferSplit);
           if (i == -1) {
             break;
           }
           let bufferTemp = Buffer.from(bufferInput);
-          try {
-            let tx = Transaction.fromRlpSerializedTx(bufferTemp.slice(0, i));
-            total++;
-            console.log(total);
-            console.log(tx);
-            if (batch.push(tx) > 1024) {
-              loadBath(batch);
-              batch = [];
-            }
-            //console.log(batch);
-          } catch (err) {
-            break;
+          let tx = Transaction.fromRlpSerializedTx(bufferTemp.slice(0, i));
+          total++;
+          batch.push(tx);
+          if (batch.length > 1024) {
+            loadBath(batch);
+            batch = [];
           }
           bufferInput = bufferInput.slice(i + bufferSplit.length);
         }
-        resolve();
-      } catch (err) {
-        this.emit('error', err);
-      }
-    });
+        if (batch.length > 0) {
+          loadBath(batch);
+        }
+      });
 
-    inputer.on('end', function () {
-      console.log('Loaded local transaction journal', 'transactions', total, 'dropped', dropped);
-    });
+      inputer.on('end', function () {
+        console.log('Loaded local transaction journal', 'transactions', total, 'dropped', dropped);
+      });
+    } catch (err) {
+      this.emit(err);
+    }
   }
 
   insert(tx: Transaction) {
     if (!this.writer) {
       throw new Error('no active journal');
     }
-    return new Promise<void>((resolve) => {
-      this.writer.write(Buffer.concat([tx.serialize(), Buffer.from('\r\n')]), (err) => {
-        if (err) {
-          rejects;
-        }
-        resolve();
-      });
+    return new Promise<void>((resolve, reject) => {
+      if (this.writer) {
+        this.writer.write(Buffer.concat([tx.serialize(), bufferSplit]), (err) => {
+          if (err) {
+            reject(err);
+          }
+          resolve();
+        });
+      }
     });
   }
 
-  rotate(all: Map<Address, Transaction[]>) {
+  async rotate(all: Map<Address, Transaction[]>) {
     if (this.writer) {
-      this.writer.close();
+      await new Promise((r) => {
+        this.writer!.end(r);
+      });
+      this.writer = undefined;
     }
     try {
       let output = fs.createWriteStream(this.path + '.new');
-    } catch (err) {
-      this.emit('error', err);
-    }
-    let output = fs.createWriteStream(this.path + '.new');
-    let journaled = 0;
-    all.forEach((val, key) => {
-      for (let tx of val) {
-        try {
-          output.write(Buffer.concat([tx.serialize(), Buffer.from('\r\n')]), (err) => {
-            if (err) {
-              console.log(err);
-            }
-          });
-        } catch (err) {
-          output.end();
-          this.emit('error', err);
+      let journaled = 0;
+      let key: Address;
+      let val: Transaction[];
+      let array: Promise<any>[] = [];
+      for ([key, val] of all) {
+        for (let tx of val) {
+          array.push(
+            new Promise<void>((resolve, reject) => {
+              output.write(Buffer.concat([tx.serialize(), bufferSplit]), (err) => {
+                if (err) {
+                  console.log(err);
+                }
+                resolve();
+              });
+            })
+          );
         }
+        journaled += val.length;
       }
-      journaled += val.length;
-    });
-    output.end();
+      await Promise.all(array).catch((err) => {
+        this.emit(err);
+      });
+      output.end();
 
-    try {
+      fs.unlinkSync(this.path);
       fs.renameSync(this.path + '.new', this.path);
+      this.writer = fs.createWriteStream(this.path, { flags: 'a' });
+      console.log('Regenerated local transaction journal', 'transactions', journaled, 'accounts', Array.from(all.keys()).length);
     } catch (err) {
-      this.emit('error', err);
+      console.log(err);
+      this.emit(err);
     }
-    try {
-      this.writer = fs.createWriteStream(this.path);
-    } catch (err) {
-      this.emit('error', err);
-    }
-    console.log('Regenerated local transaction journal', 'transactions', journaled, 'accounts', all.keys.length);
   }
 
   close() {
-    this.writer.end();
+    if (this.writer) {
+      this.writer.end();
+      this.writer = undefined;
+    }
     return;
   }
 }
-
-let txjonunal = new jonunal('/Users/bijianing/Desktop/jstest.txt');
-const unsignedTx1 = Transaction.fromTxData(
-  {
-    gasLimit: '0x5208',
-    gasPrice: '0x01',
-    nonce: 1,
-    to: '0xd1e52f6eacbb95f5f8512ff129cbd6360e549b0b',
-    value: '0x01'
-  },
-  { common: undefined }
-);
-const unsignedTx2 = Transaction.fromTxData(
-  {
-    gasLimit: '0x5209',
-    gasPrice: '0x02',
-    nonce: 2,
-    to: '0xd1e52f6eacbb95f5f8512ff129cbd6360e549b0b',
-    value: '0x02'
-  },
-  { common: undefined }
-);
-const unsignedTx3 = Transaction.fromTxData(
-  {
-    gasLimit: '0x5210',
-    gasPrice: '0x03',
-    nonce: 3,
-    to: '0x3289621709f5b35d09b4335e129907ac367a0593',
-    value: '0x03'
-  },
-  { common: undefined }
-);
-let address1 = Address.fromString('0x3289621709f5b35d09b4335e129907ac367a0593');
-let testmap = new Map([[address1, [unsignedTx1, unsignedTx2]]]);
-//txjonunal.rotate(testmap);
-//txjonunal.insert(unsignedTx3);
-//txjonunal.insert(unsignedTx1);
-txjonunal.rotate(testmap);
-txjonunal.load(function (num) {
-  console.log(num);
-});

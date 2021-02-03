fb0a6aab419b1d4d232a1f20c310cd0d0b6520bc
samlior <samlior@foxmail.com>
2021-02-03 11:14:18 +0800

    chore: add lock for worker

--- packages/gxchain-core/src/miner/worker.ts
+++ packages/gxchain-core/src/miner/worker.ts
@@ -4,11 +4,21 @@ import { Transaction } from '@gxchain2/tx';
 import { PendingTxMap } from '@gxchain2/tx-pool';
 import VM from '@gxchain2/vm';
 import { RunTxResult } from '@gxchain2/vm/dist/runTx';
+import { SemaphoreLock } from '@gxchain2/utils';
 import { Node } from '../node';
 
 export class Worker {
   private readonly node: Node;
   private readonly initPromise: Promise<void>;
+  private readonly lock = new SemaphoreLock<BN>((a, b) => {
+    if (a.lt(b)) {
+      return -1;
+    }
+    if (a.gt(b)) {
+      return 1;
+    }
+    return 0;
+  });
   private vm!: VM;
   private txs: Transaction[] = [];
   private header!: BlockHeader;
@@ -33,34 +43,50 @@ export class Worker {
   }
 
   private async _newBlock(block: Block) {
-    if (this.vm) {
-      await this.vm.stateManager.revert();
+    if (!(await this.lock.compareLock(block.header.number))) {
+      return;
     }
-    this.txs = [];
-    this.gasUsed = new BN(0);
-    this.header = BlockHeader.fromHeaderData(
-      {
-        coinbase: this.node.coinbase,
-        difficulty: '0x1',
-        gasLimit: block.header.gasLimit,
-        nonce: '0x0102030405060708',
-        number: block.header.number.addn(1),
-        parentHash: block.header.hash(),
-        uncleHash: '0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347'
-      },
-      { common: this.node.common }
-    );
-    await (this.vm = await this.node.getVM(block.header.stateRoot)).stateManager.checkpoint();
-    await this.commit(await this.node.txPool.getPendingMap());
+    try {
+      if (this.vm) {
+        await this.vm.stateManager.revert();
+      }
+      this.txs = [];
+      this.gasUsed = new BN(0);
+      this.header = BlockHeader.fromHeaderData(
+        {
+          coinbase: this.node.coinbase,
+          difficulty: '0x1',
+          gasLimit: block.header.gasLimit,
+          nonce: '0x0102030405060708',
+          number: block.header.number.addn(1),
+          parentHash: block.header.hash(),
+          uncleHash: '0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347'
+        },
+        { common: this.node.common }
+      );
+      await (this.vm = await this.node.getVM(block.header.stateRoot)).stateManager.checkpoint();
+      await this.commit(await this.node.txPool.getPendingMap());
+      this.lock.release();
+    } catch (err) {
+      this.lock.release();
+      throw err;
+    }
   }
 
   async addTxs(map: Map<Buffer, Transaction[]>) {
     await this.initPromise;
-    const pendingMap = new PendingTxMap();
-    for (const [sender, sortedTxs] of map) {
-      pendingMap.push(sender, sortedTxs);
+    await this.lock.lock();
+    try {
+      const pendingMap = new PendingTxMap();
+      for (const [sender, sortedTxs] of map) {
+        pendingMap.push(sender, sortedTxs);
+      }
+      await this.commit(pendingMap);
+      this.lock.release();
+    } catch (err) {
+      this.lock.release();
+      throw err;
     }
-    await this.commit(pendingMap);
   }
 
   async getPendingBlock() {

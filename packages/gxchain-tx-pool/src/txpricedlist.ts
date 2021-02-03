36286dcbde1861bbbc9aede150bf99066f1f7eac
sspin <>
2021-02-03 11:24:05 +0800

    chore: add the class txpricedlist

---
+++ packages/gxchain-tx-pool/src/txpricedlist.ts
@@ -0,0 +1,24 @@
+import { Transaction } from '@gxchain2/tx';
+
+export class TxPricedList {
+  stales: number;
+  all: Map<Buffer, Buffer>;
+  remotes: Transaction[];
+  constructor(all: Map<Buffer, Buffer>, stales: number) {
+    this.all = all;
+    this.remotes = [];
+    this.stales = 0;
+  }
+
+  Put(tx: Transaction, local: Boolean) {}
+
+  Removed(count: number) {}
+
+  Cap(threshold: number) {}
+
+  Underpriced(tx: Transaction) {}
+
+  Discard(slot: number, force: Boolean) {}
+
+  Reheap() {}
+}

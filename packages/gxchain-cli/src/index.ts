4e8ef15bfe167b88435fc05bec153c9a29884d9d
samlior <samlior@foxmail.com>
2021-03-11 10:56:51 +0800

    chore: change default rpc host

--- packages/gxchain-cli/src/index.ts
+++ packages/gxchain-cli/src/index.ts
@@ -10,7 +10,7 @@ import { setLevel, logger } from '@gxchain2/utils';
 program.version('0.0.1');
 program.option('--rpc', 'open rpc server');
 program.option('--rpc-port <port>', 'rpc server port', '12358');
-program.option('--rpc-host <port>', 'rpc server host', '::1');
+program.option('--rpc-host <port>', 'rpc server host', '127.0.0.1');
 program.option('--p2p-tcp-port <port>', 'p2p server tcp port', '0');
 program.option('--p2p-ws-port <port>', 'p2p server websocket port', '0');
 program.option('--bootnodes <bootnodes...>', 'bootnodes list');

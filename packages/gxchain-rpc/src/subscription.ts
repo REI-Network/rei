7b55a1fb15898829440cbfe7504a927779f86f1a
sspin <826015751@qq.com>
2021-03-24 13:51:02 +0800

    chore: add the class Filters

--- packages/gxchain-rpc/src/subscription.ts
+++ packages/gxchain-rpc/src/subscription.ts
@@ -1,5 +1,5 @@
 import { uuidv4 } from 'uuid';
-
+import { Aborter } from '@gxchain2/utils';
 function randomIDGenetator(): string {
   return uuidv4();
 }
@@ -21,6 +21,44 @@ class Subscription {
     }
     if (this.activated) {
       ws.send(JSON.stringify(data));
+    }
+  }
+}
+
+const deadline = 5 * 60 * 1000;
+type fulter = {
+  typ: string;
+  deadline: number;
+  hashes: Buffer[];
+  logs: Buffer[];
+  s: Subscription;
+};
+
+class Filters {
+  private aborter = new Aborter();
+  private readonly initPromise: Promise<void>;
+
+  constructor() {
+    this.initPromise = this.init();
+  }
+  async abort() {
+    await this.aborter.abort();
+  }
+
+  async init() {
+    if (this.initPromise) {
+      await this.initPromise;
+      return;
+    }
+  }
+
+  private async timeoutLoop() {
+    await this.initPromise;
+    while (!this.aborter.isAborted) {
+      await this.aborter.abortablePromise(new Promise((r) => setTimeout(r, deadline)));
+      if (this.aborter.isAborted) {
+        break;
+      }
     }
   }
 }

013b9906d6e6a2d3c742d46efcecc3c41f2cf4a6
samlior <samlior@foxmail.com>
2021-03-30 16:47:40 +0800

    chore(core): fix query unindexed logs

--- packages/gxchain-core/src/bloombits/bloombitsfilter.ts
+++ packages/gxchain-core/src/bloombits/bloombitsfilter.ts
@@ -1,9 +1,9 @@
 import { Address, BN, keccak256 } from 'ethereumjs-util';
 import { constants } from '@gxchain2/common';
-import { Database } from '@gxchain2/database';
 import { Block } from '@gxchain2/block';
 import { createBNFunctionalMap, createBNFunctionalSet } from '@gxchain2/utils';
 import { Log } from '@gxchain2/receipt';
+import { Node } from '../node';
 
 export function calcBloomIndexes(data: Buffer) {
   data = keccak256(data);
@@ -46,16 +46,16 @@ function topicMatched(normalizedTopics: ((Buffer | null)[] | null)[], logTopics:
 }
 
 export interface BloomBitsFilterOptions {
-  db: Database;
+  node: Node;
   sectionSize: number;
 }
 
 export class BloomBitsFilter {
-  private readonly db: Database;
+  private readonly node: Node;
   private readonly sectionSize: number;
 
   constructor(options: BloomBitsFilterOptions) {
-    this.db = options.db;
+    this.node = options.node;
     this.sectionSize = options.sectionSize;
   }
 
@@ -74,7 +74,7 @@ export class BloomBitsFilter {
   private async checkMatches(block: Block, addresses: Address[], topics: ((Buffer | null)[] | null)[]) {
     const logs: Log[] = [];
     for (const tx of block.transactions) {
-      const receipt = await this.db.getReceipt(tx.hash());
+      const receipt = await this.node.db.getReceipt(tx.hash());
       for (const log of receipt.logs) {
         if (addresses.length > 0 && addresses.findIndex((addr) => addr.buf.equals(log.address)) === -1) {
           continue;
@@ -105,75 +105,78 @@ export class BloomBitsFilter {
       }
     }
 
-    let maxSection = await this.db.getStoredSectionCount();
-    maxSection = maxSection ? maxSection : new BN(0);
-    let fromSection = from.divn(constants.BloomBitsBlocks);
-    let toSection = to.divn(constants.BloomBitsBlocks);
-    fromSection = fromSection.gt(maxSection) ? maxSection : fromSection;
-    toSection = toSection.gt(maxSection) ? maxSection : toSection;
-    for (const section = fromSection.clone(); section.lte(toSection); section.iaddn(1)) {
-      // create a map to record checked block numbers.
-      const checkedNums = createBNFunctionalSet();
-      // create a map to cache the bit set of each bit.
-      const bitsCache = new Map<number, Buffer>();
-      // create a map to cache the header hash of each section.
-      const headCache = createBNFunctionalMap<Buffer>();
-      const getSenctionHash = async (section: BN) => {
-        let hash = headCache.get(section);
-        if (!hash) {
-          hash = (await this.db.getCanonicalHeader(section.addn(1).muln(this.sectionSize).subn(1))).hash();
-          headCache.set(section, hash);
-        }
-        return hash;
-      };
-      const getBits = async (bloom: number[], section: BN) => {
-        const bitsArray: Buffer[] = [];
-        for (const bit of bloom) {
-          let bits = bitsCache.get(bit);
-          if (!bits) {
-            bits = (await this.db.getBloomBits(bit, section, await getSenctionHash(section))) as Buffer;
-            bitsCache.set(bit, bits);
+    const latestHeader = await this.node.blockchain.getLatestHeader();
+    let maxSection = await this.node.db.getStoredSectionCount();
+    if (maxSection !== undefined) {
+      // query indexed logs.
+      let fromSection = from.divn(constants.BloomBitsBlocks);
+      let toSection = to.divn(constants.BloomBitsBlocks);
+      fromSection = fromSection.gt(maxSection) ? maxSection : fromSection;
+      toSection = toSection.gt(maxSection) ? maxSection : toSection;
+      for (const section = fromSection.clone(); section.lte(toSection); section.iaddn(1)) {
+        // create a map to record checked block numbers.
+        const checkedNums = createBNFunctionalSet();
+        // create a map to cache the bit set of each bit.
+        const bitsCache = new Map<number, Buffer>();
+        // create a map to cache the header hash of each section.
+        const headCache = createBNFunctionalMap<Buffer>();
+        const getSenctionHash = async (section: BN) => {
+          let hash = headCache.get(section);
+          if (!hash) {
+            hash = (await this.node.db.getCanonicalHeader(section.addn(1).muln(this.sectionSize).subn(1))).hash();
+            headCache.set(section, hash);
           }
-          bitsArray.push(bits);
-        }
-        return bitsArray.reduce((a, b) => {
-          for (let i = 0; i < b.length; i++) {
-            b[i] &= a[i];
+          return hash;
+        };
+        const getBits = async (bloom: number[], section: BN) => {
+          const bitsArray: Buffer[] = [];
+          for (const bit of bloom) {
+            let bits = bitsCache.get(bit);
+            if (!bits) {
+              bits = (await this.node.db.getBloomBits(bit, section, await getSenctionHash(section))) as Buffer;
+              bitsCache.set(bit, bits);
+            }
+            bitsArray.push(bits);
           }
-          return b;
-        });
-      };
+          return bitsArray.reduce((a, b) => {
+            for (let i = 0; i < b.length; i++) {
+              b[i] &= a[i];
+            }
+            return b;
+          });
+        };
 
-      // the start block number of this section.
-      const sectionStart = section.muln(this.sectionSize);
-      // the end block number of this section.
-      const sectionEnd = section.addn(1).muln(this.sectionSize);
-      // calculate the start block number for check.
-      const fromBlock = from.lt(sectionStart) ? sectionStart : from;
-      // calculate the end block number for check.
-      const toBlock = to.gt(sectionEnd) ? sectionEnd : to;
-      for (const bloom of blooms) {
-        // query the bits set of this bit of this section from database.
-        const bits = await getBits(bloom, section);
-        for (const num = fromBlock.clone(); num.lt(toBlock); num.iaddn(1)) {
-          if (!checkedNums.has(num) && this.checkSingleNumber(bits, sectionStart, num)) {
-            checkedNums.add(num.clone());
-            append(await this.filterBlock(num, addresses, topics));
+        // the start block number of this section.
+        const sectionStart = section.muln(this.sectionSize);
+        // the end block number of this section.
+        const sectionEnd = section.addn(1).muln(this.sectionSize);
+        // calculate the start block number for check.
+        const fromBlock = from.lt(sectionStart) ? sectionStart : from;
+        // calculate the end block number for check.
+        const toBlock = to.gt(sectionEnd) ? sectionEnd : to;
+        for (const bloom of blooms) {
+          // query the bits set of this bit of this section from database.
+          const bits = await getBits(bloom, section);
+          for (const num = fromBlock.clone(); num.lt(toBlock); num.iaddn(1)) {
+            if (!checkedNums.has(num) && this.checkSingleNumber(bits, sectionStart, num)) {
+              checkedNums.add(num.clone());
+              append(await this.filterBlock(num, addresses, topics));
+            }
           }
         }
       }
     }
 
     // query unindexed logs.
-    const maxIndexedBlockNumber = maxSection.addn(1).muln(constants.BloomBitsBlocks).subn(1);
-    for (const num = maxIndexedBlockNumber.addn(1); num.lte(to); num.iaddn(1)) {
+    const maxIndexedBlockNumber = maxSection ? maxSection.addn(1).muln(constants.BloomBitsBlocks).subn(1) : new BN(0);
+    for (const num = maxIndexedBlockNumber.addn(1); num.lte(to) && num.lte(latestHeader.number); num.iaddn(1)) {
       append(await this.filterBlock(num, addresses, topics));
     }
     return logs;
   }
 
   async filterBlock(blockHashOrNumber: Buffer | BN | number, addresses: Address[], topics: ((Buffer | null)[] | null)[]) {
-    const block = await this.db.getBlock(blockHashOrNumber);
+    const block = await this.node.db.getBlock(blockHashOrNumber);
     const logs = await this.checkMatches(block, addresses, topics);
     logs.forEach((log) => (log.removed = false));
     return logs;

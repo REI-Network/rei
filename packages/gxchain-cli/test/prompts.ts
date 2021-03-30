57554214c75759e033b3e708d31a84ac62e65875
samlior <samlior@foxmail.com>
2021-03-30 14:01:29 +0800

    chore(cli): use getStoredSectionCount instead of low level db operate

--- packages/gxchain-cli/test/prompts.ts
+++ packages/gxchain-cli/test/prompts.ts
@@ -141,7 +141,7 @@ const handler: {
     await node.txPool.ls();
   },
   lsscount: async (node: Node) => {
-    console.log('scount', new BN(await node.rawdb.get('scount')).toString());
+    console.log('scount', new BN(await node.db.getStoredSectionCount()).toString());
   },
   filterblock: async (node: Node, number: string, addresses: string, topics: string) => {
     const { addressArray, topicArray } = parseAddressAndTopic(addresses, topics);

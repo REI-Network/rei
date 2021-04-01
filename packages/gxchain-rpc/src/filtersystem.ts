4742f58bcf3ef5f1d04e3dc8ce86e9866f0c8c37
sspin <826015751@qq.com>
2021-04-01 10:59:51 +0800

    chore: remove function delete

--- packages/gxchain-rpc/src/filtersystem.ts
+++ packages/gxchain-rpc/src/filtersystem.ts
@@ -28,14 +28,13 @@ export class FilterSystem {
 
   private readonly initPromise: Promise<void>;
 
-  private readonly wsPendingTransactionsMap: Map<string, FilterInfo> = new Map();
-  private readonly wsPendingLogsMap: Map<string, FilterInfo> = new Map();
-  private readonly wsLogMap: Map<string, FilterInfo> = new Map();
   private readonly wsHeadMap: Map<string, FilterInfo> = new Map();
-  private readonly httpPendingTransactionsMap: Map<string, FilterInfo> = new Map();
-  private readonly httpPendingLogsMap: Map<string, FilterInfo> = new Map();
-  private readonly httpLogMap: Map<string, FilterInfo> = new Map();
+  private readonly wsLogMap: Map<string, FilterInfo> = new Map();
+  private readonly wsPendingTransactionsMap: Map<string, FilterInfo> = new Map();
+  private readonly wsSyncingMap: Map<string, FilterInfo> = new Map();
   private readonly httpHeadMap: Map<string, FilterInfo> = new Map();
+  private readonly httpLogMap: Map<string, FilterInfo> = new Map();
+  private readonly httpPendingTransactionsMap: Map<string, FilterInfo> = new Map();
 
   constructor() {
     this.initPromise = this.init();
@@ -71,35 +70,27 @@ export class FilterSystem {
       this.cycleDelete(this.httpLogMap);
       this.cycleDelete(this.httpPendingTransactionsMap);
       this.cycleDelete(this.httpLogMap);
-      this.cycleDelete(this.httpPendingLogsMap);
     }
   }
 
   wsSubscibe(client: WsClient, queryInfo: FilterQuery): string {
-    let hashes: Buffer[] = [];
-    let logs: Log[] = [];
     const uid = uuidv4();
-    let filterInstance = { type: queryInfo.type, createtime: Date.now(), hashes: hashes, logs: logs, queryInfo: queryInfo, notify: client.send };
+    const filterInstance = { type: queryInfo.type, createtime: Date.now(), hashes: [], logs: [], queryInfo: queryInfo, notify: client.send };
     switch (queryInfo.type) {
-      case 'LogsSubscription': {
+      case 'newHeads': {
+        this.wsHeadMap.set(uid, filterInstance);
+        break;
+      }
+      case 'logs': {
         this.wsLogMap.set(uid, filterInstance);
         break;
       }
-      case 'PendingLogsSubscription': {
-        this.wsPendingLogsMap.set(uid, filterInstance);
-        break;
-      }
-      case 'MinedAndPendingLogsSubscription': {
-        this.wsLogMap.set(uid, filterInstance);
-        this.wsPendingLogsMap.set(uid, filterInstance);
-        break;
-      }
-      case 'PendingTransactionsSubscription': {
+      case 'newPendingTransactions': {
         this.wsPendingTransactionsMap.set(uid, filterInstance);
         break;
       }
-      case 'BlocksSubscription': {
-        this.wsHeadMap.set(uid, filterInstance);
+      case 'syncing': {
+        this.wsSyncingMap.set(uid, filterInstance);
         break;
       }
     }
@@ -108,68 +99,49 @@ export class FilterSystem {
 
   httpSubscribe(queryInfo: FilterQuery): string {
     const uid = uuidv4();
-    let hashes: Buffer[] = [];
-    let logs: Log[] = [];
-    let filterInstance = { type: queryInfo.type, createtime: Date.now(), hashes: hashes, logs: logs, queryInfo: queryInfo };
+    const filterInstance = { type: queryInfo.type, createtime: Date.now(), hashes: [], logs: [], queryInfo: queryInfo };
     switch (queryInfo.type) {
-      case 'LogsSubscription': {
-        this.httpLogMap.set(uid, filterInstance);
-        break;
-      }
-      case 'PendingLogsSubscription': {
-        this.httpPendingLogsMap.set(uid, filterInstance);
-        break;
-      }
-      case 'MinedAndPendingLogsSubscription': {
-        this.httpLogMap.set(uid, filterInstance);
-        this.httpPendingLogsMap.set(uid, filterInstance);
-        break;
-      }
-      case 'PendingTransactionsSubscription': {
-        this.httpPendingTransactionsMap.set(uid, filterInstance);
-        break;
-      }
-      case 'BlocksSubscription': {
+      case 'newHeads': {
         this.httpHeadMap.set(uid, filterInstance);
+        break;
+      }
+      case 'logs': {
+        this.httpLogMap.set(uid, filterInstance);
+        break;
+      }
+      case 'newPendingTransactions': {
+        this.httpPendingTransactionsMap.set(uid, filterInstance);
         break;
       }
     }
     return uid;
   }
 
-  private removeFromMap(id: string, map: Map<string, FilterInfo>) {
-    if (map.has(id)) {
-      map.delete(id);
-    }
-  }
-
   wsUnsubscribe(id: string) {
-    this.removeFromMap(id, this.wsHeadMap);
-    this.removeFromMap(id, this.wsLogMap);
-    this.removeFromMap(id, this.wsPendingLogsMap);
-    this.removeFromMap(id, this.wsPendingTransactionsMap);
+    this.wsHeadMap.delete(id);
+    this.wsLogMap.delete(id);
+    this.wsPendingTransactionsMap.delete(id);
+    this.wsSyncingMap.delete(id);
   }
 
   httpUnsubscribe(id: string) {
-    this.removeFromMap(id, this.httpHeadMap);
-    this.removeFromMap(id, this.httpLogMap);
-    this.removeFromMap(id, this.httpPendingLogsMap);
-    this.removeFromMap(id, this.httpPendingTransactionsMap);
+    this.httpHeadMap.delete(id);
+    this.httpLogMap.delete(id);
+    this.httpPendingTransactionsMap.delete(id);
   }
 
   private changed(id: string, map: Map<string, FilterInfo>, logorhash: boolean) {
     const filterInfo = map.get(id);
+    if (!filterInfo) {
+      return;
+    }
     if (logorhash) {
-      const info = filterInfo?.logs;
-      if (filterInfo?.logs) {
-        filterInfo.logs = [];
-      }
+      const info = filterInfo.logs;
+      filterInfo.logs = [];
       return info;
     } else {
       const info = filterInfo?.hashes;
-      if (filterInfo?.hashes) {
-        filterInfo.hashes = [];
-      }
+      filterInfo.hashes = [];
       return info;
     }
   }
@@ -177,24 +149,14 @@ export class FilterSystem {
   httpFilterChanged(id: string, type: string) {
     // if type === 'PendingTransactions';
     switch (type) {
-      case 'LogsSubscription': {
+      case 'newHeads': {
+        return this.changed(id, this.httpHeadMap, false);
+      }
+      case 'logs': {
         return this.changed(id, this.httpLogMap, true);
       }
-      case 'PendingLogsSubscription': {
-        return this.changed(id, this.httpPendingLogsMap, true);
-        break;
-      }
-      case 'MinedAndPendingLogsSubscription': {
-        return [this.changed(id, this.httpLogMap, true), this.changed(id, this.httpPendingLogsMap, true)];
-        break;
-      }
-      case 'PendingTransactionsSubscription': {
+      case 'newPendingTransactions': {
         return this.changed(id, this.httpPendingTransactionsMap, false);
-        break;
-      }
-      case 'BlocksSubscription': {
-        return this.changed(id, this.httpHeadMap, false);
-        break;
       }
     }
   }
@@ -220,4 +182,20 @@ export class FilterSystem {
       filterInfo.hashes.push(hash);
     }
   }
+
+  // private includes()
+
+  // filterLogs(logs: Log[], filterinstance: FilterInfo) {
+  //   for (const log of logs) {
+  //     if (filterinstance.queryInfo.fromBlock >= 0 && filterinstance.queryInfo.fromBlock > log.blockNumber!.toNumber()) {
+  //       continue;
+  //     }
+  //     if (filterinstance.queryInfo.toBlock >= 0 && filterinstance.queryInfo.toBlock < log.blockNumber!.toNumber()) {
+  //       continue;
+  //     }
+  //     if (filterinstance.queryInfo.addresses.length > 0 && ) {
+  //       continue
+  //     }
+  //   }
+  // }
 }

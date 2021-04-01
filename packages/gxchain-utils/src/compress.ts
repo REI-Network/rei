342daef410c89b80f1daba91d39de52b207113c8
sspin <826015751@qq.com>
2021-04-01 18:22:45 +0800

    chore: add the DecompressBytes and bitsetDecodePartialBytes function

--- packages/gxchain-utils/src/compress.ts
+++ packages/gxchain-utils/src/compress.ts
@@ -1,3 +1,8 @@
+const errMissingData = new Error('missing bytes on input');
+const errUnreferencedData = new Error('extra bytes on input');
+const errExceededTarget = new Error('target data size exceeded');
+const errZeroContent = new Error('zero byte in input content');
+
 function compressBytes(data: number[]): number[] | any {
   const out = bitsetEncodeBytes(data);
   if (out.length < data.length) {
@@ -41,4 +46,62 @@ function bitsetEncodeBytes(data: number[]): number[] | any {
   bitsetEncodeBytes(nonZeroBitset);
   nonZeroBitset.push(...nonZeroBytes);
   return nonZeroBitset;
+}
+
+function DecompressBytes(data: number[], target: number): number[] {
+  if (data.length > target) {
+    throw errExceededTarget;
+  }
+  if (data.length == target) {
+    const cpy = data;
+    return cpy;
+  }
+  return [];
+  //return bitsetDecodeBytes(data, target);
+}
+
+// function bitsetDecodeBytes(data: number[], target: number): number[] {
+//   //const [out,size] =
+// }
+
+function bitsetDecodePartialBytes(data: number[], target: number): [number[], number] {
+  if (target == 0) {
+    return [[], 0];
+  }
+  let i = 0;
+  let decomp: number[] = [];
+  while (i < target) {
+    decomp[i] = 0;
+    i++;
+  }
+  if (data.length == 0) {
+    return [decomp, 0];
+  }
+  if (target == 1) {
+    decomp[0] = data[0];
+    if (data[0] != 0) {
+      return [decomp, 1];
+    }
+    return [decomp, 0];
+  }
+
+  let [nonZeroBitset, ptr] = bitsetDecodePartialBytes(data, (target + 7) / 8);
+  return [[], ptr];
+  for (let i = 0; i < 8 * nonZeroBitset.length; i++) {
+    const judement = nonZeroBitset[i / 8] & (1 << (7 - (i % 8)));
+    if (judement != 0) {
+      if (ptr >= data.length) {
+        throw errMissingData;
+      }
+      if (i >= decomp.length) {
+        throw errExceededTarget;
+      }
+      if (data[ptr] == 0) {
+        throw errZeroContent;
+      }
+      decomp[i] = data[ptr];
+      ptr++;
+    }
+  }
+  return [decomp, ptr];
 }

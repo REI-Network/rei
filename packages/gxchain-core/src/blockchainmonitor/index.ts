7ee3ab33a6a9f8366341ed95ee2f843a7aee149b
samlior <samlior@foxmail.com>
2021-04-02 16:21:39 +0800

    chore(core): improve blockchain monitor, use getReceiptByHashAndNumber for rollback receipt

--- packages/gxchain-core/src/blockchainmonitor/index.ts
+++ packages/gxchain-core/src/blockchainmonitor/index.ts
@@ -6,6 +6,9 @@ import { createBufferFunctionalMap, logger } from '@gxchain2/utils';
 import { Log } from '@gxchain2/receipt';
 import { Node } from '../node';
 
+// record block hash and block number for quering receipt.
+type TransactionInfo = { tx: Transaction; blockHash: Buffer; blockNumber: BN };
+
 export declare interface BlockchainMonitor {
   on(event: 'logs' | 'removedLogs', listener: (logs: Log[]) => void): this;
   on(event: 'newHeads', listener: (hashes: Buffer[]) => void): this;
@@ -60,45 +63,54 @@ export class BlockchainMonitor extends EventEmitter {
       const originalNewBlock = block;
       const newHeads: BlockHeader[] = [];
       let oldBlock = await getBlock(this.currentHeader.hash(), this.currentHeader.number);
-      const discarded = createBufferFunctionalMap<Transaction>();
-      const included = createBufferFunctionalMap<Transaction>();
+      const discarded = createBufferFunctionalMap<TransactionInfo>();
+      const included = createBufferFunctionalMap<TransactionInfo>();
       while (oldBlock.header.number.gt(block.header.number)) {
+        const blockHash = oldBlock.hash();
         for (const tx of oldBlock.transactions) {
-          discarded.set(tx.hash(), tx);
+          discarded.set(tx.hash(), { tx, blockHash, blockNumber: oldBlock.header.number });
         }
         oldBlock = await getBlock(oldBlock.header.parentHash, oldBlock.header.number.subn(1));
       }
       while (block.header.number.gt(oldBlock.header.number)) {
         newHeads.push(block.header);
+        const blockHash = block.hash();
         for (const tx of block.transactions) {
-          included.set(tx.hash(), tx);
+          included.set(tx.hash(), { tx, blockHash, blockNumber: block.header.number });
         }
         block = await getBlock(block.header.parentHash, block.header.number.subn(1));
       }
       while (!oldBlock.hash().equals(block.hash()) && oldBlock.header.number.gtn(0) && block.header.number.gtn(0)) {
-        for (const tx of oldBlock.transactions) {
-          discarded.set(tx.hash(), tx);
+        {
+          const blockHash = oldBlock.hash();
+          for (const tx of oldBlock.transactions) {
+            discarded.set(tx.hash(), { tx, blockHash, blockNumber: oldBlock.header.number });
+          }
         }
         oldBlock = await getBlock(oldBlock.header.parentHash, oldBlock.header.number.subn(1));
-        newHeads.push(block.header);
-        for (const tx of block.transactions) {
-          included.set(tx.hash(), tx);
+
+        {
+          newHeads.push(block.header);
+          const blockHash = block.hash();
+          for (const tx of block.transactions) {
+            included.set(tx.hash(), { tx, blockHash, blockNumber: block.header.number });
+          }
         }
         block = await getBlock(block.header.parentHash, block.header.number.subn(1));
       }
       if (!oldBlock.hash().equals(block.hash())) {
         throw new Error('reorg failed');
       }
-      const removed: Transaction[] = [];
-      for (const tx of discarded.values()) {
-        if (!included.has(tx.hash())) {
-          removed.push(tx);
+      const removed: TransactionInfo[] = [];
+      for (const txInfo of discarded.values()) {
+        if (!included.has(txInfo.tx.hash())) {
+          removed.push(txInfo);
         }
       }
-      const rebirthed: Transaction[] = [];
-      for (const tx of included.values()) {
-        if (!discarded.has(tx.hash())) {
-          rebirthed.push(tx);
+      const rebirthed: TransactionInfo[] = [];
+      for (const txInfo of included.values()) {
+        if (!discarded.has(txInfo.tx.hash())) {
+          rebirthed.push(txInfo);
         }
       }
       // reset current header.
@@ -106,21 +118,39 @@ export class BlockchainMonitor extends EventEmitter {
       // emit event.
       if (removed.length > 0) {
         let removedLogs: Log[] = [];
-        for (const tx of removed) {
-          const receipt = await this.node.db.getReceipt(tx.hash());
-          receipt.logs.forEach((log) => (log.removed = true));
-          removedLogs = removedLogs.concat(receipt.logs);
+        for (const txInfo of removed) {
+          try {
+            const receipt = await this.node.db.getReceiptByHashAndNumber(txInfo.tx.hash(), txInfo.blockHash, txInfo.blockNumber);
+            receipt.logs.forEach((log) => (log.removed = true));
+            removedLogs = removedLogs.concat(receipt.logs);
+          } catch (err) {
+            if (err.type === 'NotFoundError') {
+              continue;
+            }
+            throw err;
+          }
         }
-        this.emit('removedLogs', removedLogs);
+        if (removedLogs.length > 0) {
+          this.emit('removedLogs', removedLogs);
+        }
       }
       if (rebirthed.length > 0) {
         let logs: Log[] = [];
-        for (const tx of rebirthed) {
-          const receipt = await this.node.db.getReceipt(tx.hash());
-          receipt.logs.forEach((log) => (log.removed = false));
-          logs = logs.concat(receipt.logs);
+        for (const txInfo of rebirthed) {
+          try {
+            const receipt = await this.node.db.getReceipt(txInfo.tx.hash());
+            receipt.logs.forEach((log) => (log.removed = false));
+            logs = logs.concat(receipt.logs);
+          } catch (err) {
+            if (err.type === 'NotFoundError') {
+              continue;
+            }
+            throw err;
+          }
         }
-        this.emit('logs', logs);
+        if (logs.length > 0) {
+          this.emit('logs', logs);
+        }
       }
       if (newHeads.length > 0) {
         newHeads.sort((a, b) => {

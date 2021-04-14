ed30c5f6d51daacb4fb5c9f4c0e6d763638634d9
samlior <samlior@foxmail.com>
2021-04-14 17:47:17 +0800

    feat(core): add tracer for core

---
+++ packages/gxchain-core/src/tracer/debug/structlogdebug.ts
@@ -0,0 +1,88 @@
+import { DebugOpts, InterpreterStep, VmError } from '@gxchain2/vm';
+import { Address, BN } from 'ethereumjs-util';
+import { TraceConfig } from '../tracer';
+
+export type StructLog = {
+  depth: number;
+  error: string;
+  gas: number;
+  gasCost: number;
+  memory: null | string[];
+  op: string;
+  pc: number;
+  stack: string[];
+  storage: {
+    [key: string]: string;
+  };
+};
+
+export class StructLogDebug implements DebugOpts {
+  hash?: Buffer;
+  config: TraceConfig;
+  logs: StructLog[] = [];
+  output!: Buffer;
+  gasUsed!: BN;
+  failed: boolean = false;
+
+  constructor(config: TraceConfig, hash?: Buffer) {
+    this.config = config;
+    this.hash = hash;
+  }
+
+  private captureLog(step: InterpreterStep, err?: string) {
+    let memory: null | string[] = null;
+    if (!this.config.disableMemory && step.memoryWordCount.gtn(0)) {
+      const memoryLength = new BN(step.memory.length).div(step.memoryWordCount).toNumber();
+      memory = [];
+      for (let i = 0; i < step.memoryWordCount.toNumber(); i++) {
+        memory.push(step.memory.slice(i * memoryLength, (i + 1) * memoryLength).toString('hex'));
+      }
+    }
+    const log: StructLog = {
+      depth: step.depth,
+      error: err || '',
+      gas: step.gasLeft.toNumber(),
+      gasCost: step.opcode.fee,
+      memory,
+      op: step.opcode.name,
+      pc: step.pc,
+      stack: !this.config.disableStack ? step.stack.map((bn) => bn.toString('hex')) : [],
+      storage: !this.config.disableStorage ? {} : {}
+    };
+    this.logs.push(log);
+  }
+
+  captureStart(from: Address, create: boolean, input: Buffer, gas: BN, value: BN, to?: Address) {}
+
+  captureState(step: InterpreterStep) {
+    this.captureLog(step);
+  }
+
+  captureFault(step: InterpreterStep, err: any) {
+    let errString: string;
+    if (err instanceof VmError) {
+      errString = err.errorType;
+    } else if (err instanceof Error) {
+      errString = err.message;
+    } else if (typeof err === 'string') {
+      errString = err;
+    } else {
+      errString = 'unkonw error';
+    }
+    this.captureLog(step, errString);
+  }
+
+  captureEnd(output: Buffer, gasUsed: BN, time: number) {
+    this.output = output;
+    this.gasUsed = gasUsed.clone();
+  }
+
+  result() {
+    return {
+      failed: this.failed,
+      gas: this.gasUsed.toNumber(),
+      returnValue: this.output.toString('hex'),
+      structLogs: this.logs
+    };
+  }
+}
---
+++ packages/gxchain-core/src/tracer/tracer.ts
@@ -0,0 +1,64 @@
+import { BN } from 'ethereumjs-util';
+import { Block } from '@gxchain2/block';
+import { DebugOpts } from '@gxchain2/vm';
+import { Node } from '../node';
+import { StructLogDebug } from './debug/structlogdebug';
+
+export interface TracerOptions {
+  node: Node;
+}
+
+export interface TraceConfig {
+  disableStorage?: boolean;
+  disableMemory?: boolean;
+  disableStack?: boolean;
+  tracer?: string;
+  timeout?: string;
+}
+
+export class Tracer {
+  private readonly node: Node;
+
+  constructor(options: TracerOptions) {
+    this.node = options.node;
+  }
+
+  private createDebugImpl(config: TraceConfig, hash?: Buffer): DebugOpts {
+    return new StructLogDebug(config, hash);
+  }
+
+  private debugResult(debug: DebugOpts) {
+    if (debug instanceof StructLogDebug) {
+      return debug.result();
+    }
+    throw new Error('unknow error');
+  }
+
+  private async _traceBlock(block: Block, config: TraceConfig, hash?: Buffer) {
+    if (block.header.number.eqn(0)) {
+      throw new Error('invalid block number, 0');
+    }
+    const parent = await this.node.db.getBlockByHashAndNumber(block.header.parentHash, block.header.number.subn(1));
+    const wvm = await this.node.getWrappedVM(parent.header.stateRoot);
+    const debug = this.createDebugImpl(config, hash);
+    await wvm.runBlock({ block, debug });
+    return this.debugResult(debug);
+  }
+
+  async traceBlockByNumber(number: BN, config: TraceConfig) {
+    return await this._traceBlock(await this.node.db.getBlock(number), config);
+  }
+
+  async traceBlockByHash(hash: Buffer, config: TraceConfig) {
+    return await this._traceBlock(await this.node.db.getBlock(hash), config);
+  }
+
+  async traceBlock(blockRlp: Buffer, config: TraceConfig) {
+    return await this._traceBlock(Block.fromRLPSerializedBlock(blockRlp, { common: this.node.common }), config);
+  }
+
+  async traceTx(hash: Buffer, config: TraceConfig) {
+    const wtx = await this.node.db.getWrappedTransaction(hash);
+    return await this._traceBlock(await this.node.db.getBlockByHashAndNumber(wtx.extension.blockHash!, wtx.extension.blockNumber!), config, hash);
+  }
+}

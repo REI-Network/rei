81ef1f8a99adb839219f2e6744ba608e31c6fbda
sspin <826015751@qq.com>
2021-04-26 17:37:52 +0800

    chore: add filecache.ts

---
+++ packages/gxchain-cli/src/filecache.ts
@@ -0,0 +1,42 @@
+import fs from 'fs';
+import path from 'path';
+
+export class FileCache {
+  all: Array<string>;
+  lastMod: number;
+
+  constructor() {
+    this.all = new Array<string>();
+    this.lastMod = Date.now();
+  }
+
+  scan(keydir: string) {
+    const filestmp = fs.readdirSync(keydir);
+    const files = filestmp.filter((item) => !/(^|\/)\.[^\/\.]/g.test(item));
+    if (files.length == 0) {
+      return;
+    }
+    let all = new Array<string>();
+    let mods = new Array<string>();
+    let newLastmode = Date.now();
+    for (const fi of files) {
+      let pathtmp = path.join(keydir, fi);
+      all.push(pathtmp);
+      const modified = fs.statSync(fi).mtime.getTime();
+      if (modified > this.lastMod) {
+        mods.push(pathtmp);
+      }
+      if (modified > newLastmode) {
+        newLastmode = modified;
+      }
+    }
+
+    const deletes = this.all.filter((x) => !all.includes(x));
+    const creates = all.filter((x) => this.all.includes(x));
+    const updates = mods.filter((x) => !creates.includes(x));
+
+    this.all = all;
+    this.lastMod = newLastmode;
+    return [creates, deletes, updates];
+  }
+}

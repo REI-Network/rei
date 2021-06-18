ed7d718e448339fb0ca6c6d0cfe138f870684bb3
samlior <samlior@foxmail.com>
2021-06-18 17:30:55 +0800

    chore: update prompts, remove useless method

--- packages/gxchain-cli/test/prompts.ts
+++ packages/gxchain-cli/test/prompts.ts
@@ -6,21 +6,11 @@ import Multiaddr from 'multiaddr';
 import { Address, bufferToHex, BN } from 'ethereumjs-util';
 import { Node } from '@gxchain2/core';
 import { constants } from '@gxchain2/common';
-import { TransactionFactory } from '@gxchain2/structure';
 import { hexStringToBuffer, logger } from '@gxchain2/utils';
 import { BloomBitsFilter } from '@gxchain2/core/dist/bloombits';
 import { startNode } from '../src/start';
 import program from '../src/program';
 import { SIGINT } from '../src/process';
-
-const keyPair = new Map<string, Buffer>([
-  ['0x3289621709f5b35d09b4335e129907ac367a0593', Buffer.from('d8ca4883bbf62202904e402750d593a297b5640dea80b6d5b239c5a9902662c0', 'hex')],
-  ['0xd1e52f6eacbb95f5f8512ff129cbd6360e549b0b', Buffer.from('db0558cc5f24dd09c390a25c7958a678e7efa0f286053da5df53dcecdba2a13c', 'hex')]
-]);
-
-const getPrivateKey = (address: string): Buffer => {
-  return keyPair.get(address)!;
-};
 
 // addresses, a list of address, splited by `,`
 // topics, a list of topic, splited by `,` and each subTopic splited by `;`.
@@ -58,14 +48,6 @@ const handler: {
   lsp2p: (node: Node) => {
     for (const peer of node.networkMngr.peers) {
       logger.info(peer.peerId);
-    }
-  },
-  // batch mine block.
-  bm: async (node: Node, strCount?: string) => {
-    const count = isNaN(Number(strCount)) ? 1 : Number(strCount);
-    for (let i = 0; i < count; i++) {
-      await node.miner.mint();
-      await new Promise((r) => setTimeout(r, 1000));
     }
   },
   lsreceipt: async (node: Node, hash: string) => {
@@ -117,23 +99,6 @@ const handler: {
   lsaccount: async (node: Node, address: string) => {
     const acc = await (await node.getStateManager(node.blockchain.latestBlock.header.stateRoot, node.blockchain.latestHeight)).getAccount(Address.fromString(address));
     logger.info('balance', acc.balance.toString(), 'nonce', acc.nonce.toString(), 'codeHash', acc.codeHash.toString('hex'));
-  },
-  puttx: async (node: Node, from: string, to: string, nonce?: string, gasPrice?: string) => {
-    const unsignedTx = TransactionFactory.fromTxData(
-      {
-        gasLimit: new BN(21000),
-        gasPrice: new BN(gasPrice || 1),
-        nonce: new BN(nonce || 0),
-        to,
-        value: '0x01'
-      },
-      { common: node.getCommon(node.blockchain.latestHeight) }
-    );
-    const tx = unsignedTx.sign(getPrivateKey(from));
-    const results = await node.addPendingTxs([tx]);
-    if (results[0]) {
-      logger.info('puttx', bufferToHex(tx.hash()));
-    }
   },
   lstxpool: async (node: Node) => {
     await node.txPool.ls();

dea9d77a2a00534167e03d8360314f92597b8f94
sspin <826015751@qq.com>
2021-06-29 11:22:17 +0800

    chore: Add code comments for utils/index.ts

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -5,6 +5,12 @@ interface Constructor<T = {}> {
   new (...args: any[]): T;
 }
 
+/**
+ * This function implements multiple inheritance
+ * @param mix1 The first parameter to be inherited
+ * @param mix2 The second  parameter to be inherited
+ * @returns The result after multiple inheritance
+ */
 export function mixin<T1 extends Constructor, T2 extends Constructor>(mix1: T1, mix2: T2): new (...args: any[]) => InstanceType<T1> & InstanceType<T2> {
   const mixinProps = (target, source) => {
     Object.getOwnPropertyNames(source).forEach((prop) => {
@@ -29,16 +35,35 @@ export function mixin<T1 extends Constructor, T2 extends Constructor>(mix1: T1, 
   return ctor;
 }
 
+/**
+ * Generate random integers according to the given range
+ * @param min Minimum limit
+ * @param max Maximum limit
+ * @returns The random number
+ */
 export function getRandomIntInclusive(min: number, max: number): number {
   min = Math.ceil(min);
   max = Math.floor(max);
+  if (max < min) {
+    throw new Error('The maximum value should be greater than the minimum value');
+  }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+/**
+ * Convert hex string type to buffer type
+ * @param hex Hex string to be converted
+ * @returns Converted buffer type data
+ */
 export function hexStringToBuffer(hex: string): Buffer {
   return hex.indexOf('0x') === 0 ? Buffer.from(hex.substr(2), 'hex') : Buffer.from(hex, 'hex');
 }
 
+/**
+ * Convert hex string type to BN type
+ * @param hex Hex string to be converted
+ * @returns Converted BN type data
+ */
 export function hexStringToBN(hex: string): BN {
   return hex.indexOf('0x') === 0 ? new BN(hex.substr(2), 'hex') : new BN(hex, 'hex');
 }

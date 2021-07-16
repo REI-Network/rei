0931a23722abf6af5261d50d087b426852519b29
samlior <samlior@foxmail.com>
2021-07-16 15:34:18 +0800

    chore: add removed logs test for blockchain monitor

--- packages/core/test/blockchainmonitor/blockchainmonitor.test.ts
+++ packages/core/test/blockchainmonitor/blockchainmonitor.test.ts
@@ -15,6 +15,7 @@ const privateKey = '0xd8ca4883bbf62202904e402750d593a297b5640dea80b6d5b239c5a990
 describe('BlockchainMonitor', async () => {
   let node: Node;
   let fork1: Block[];
+  let fork2: Block[];
 
   async function processBlock(block: Block) {
     const lastHeader = await node.db.getHeader(block.header.parentHash, block.header.number.subn(1));
@@ -45,37 +46,63 @@ describe('BlockchainMonitor', async () => {
     await node.accMngr.importKeyByPrivateKey(privateKey, '123');
     await node.accMngr.unlock(address, '123');
     fork1 = loadBlocksFromTestData(dirname, 'fork1', 'gxc2-testnet', hexStringToBuffer(privateKey));
+    fork2 = loadBlocksFromTestData(dirname, 'fork2', 'gxc2-testnet', hexStringToBuffer(privateKey));
   });
 
   it("should emit 'newHeads' and 'logs' event", async () => {
     const newBlockHashSet = new Set<string>();
     const eventSet = new Set<string>();
     let logs: Log[] = [];
-    node.bcMonitor.on('newHeads', (hashes) => {
+    const onNewHeads = (hashes: Buffer[]) => {
       hashes.forEach((hash) => {
         eventSet.add(hash.toString('hex'));
       });
-    });
-    node.bcMonitor.on('logs', (_logs) => {
+    };
+    const onLogs = (_logs: Log[]) => {
       logs = logs.concat(_logs);
-    });
-    for (const block of fork1) {
-      const newBlock = await processBlock(block);
-      newBlockHashSet.add(newBlock.hash().toString('hex'));
+    };
+    try {
+      node.bcMonitor.on('newHeads', onNewHeads);
+      node.bcMonitor.on('logs', onLogs);
+      for (const block of fork1) {
+        const newBlock = await processBlock(block);
+        newBlockHashSet.add(newBlock.hash().toString('hex'));
+      }
+      if (newBlockHashSet.size !== eventSet.size) {
+        console.log(newBlockHashSet, eventSet);
+        throw new Error("missing 'newHeads' event");
+      }
+      for (const hash of newBlockHashSet) {
+        newBlockHashSet.delete(hash);
+        eventSet.delete(hash);
+      }
+      if (newBlockHashSet.size !== 0 || eventSet.size !== 0) {
+        throw new Error("missing 'newHeads' event");
+      }
+      if (logs.length !== 1 || logs[0].removed !== false || !keccak256(logs[0].serialize()).equals(hexStringToBuffer('81a93f1b18562fe496865812f8cff49db870421fe889ffb11b3fc13aacc8d125'))) {
+        throw new Error('missing or invalid log');
+      }
+    } finally {
+      node.bcMonitor.off('newHeads', onNewHeads);
+      node.bcMonitor.off('logs', onLogs);
     }
-    if (newBlockHashSet.size !== eventSet.size) {
-      console.log(newBlockHashSet, eventSet);
-      throw new Error("missing 'newHeads' event");
-    }
-    for (const hash of newBlockHashSet) {
-      newBlockHashSet.delete(hash);
-      eventSet.delete(hash);
-    }
-    if (newBlockHashSet.size !== 0 || eventSet.size !== 0) {
-      throw new Error("missing 'newHeads' event");
-    }
-    if (logs.length !== 1 || !keccak256(logs[0].serialize()).equals(hexStringToBuffer('81a93f1b18562fe496865812f8cff49db870421fe889ffb11b3fc13aacc8d125'))) {
-      throw new Error('missing or invalid log');
+  });
+
+  it("should emit 'removedLogs' event", async () => {
+    let removedLogs: Log[] = [];
+    const onRemovedLogs = (_logs: Log[]) => {
+      removedLogs = removedLogs.concat(_logs);
+    };
+    try {
+      node.bcMonitor.on('removedLogs', onRemovedLogs);
+      for (const block of fork2) {
+        await processBlock(block);
+      }
+      if (removedLogs.length !== 1 || removedLogs[0].removed !== true || !keccak256(removedLogs[0].serialize()).equals(hexStringToBuffer('81a93f1b18562fe496865812f8cff49db870421fe889ffb11b3fc13aacc8d125'))) {
+        throw new Error('missing or invalid log');
+      }
+    } finally {
+      node.bcMonitor.off('removedLogs', onRemovedLogs);
     }
   });
 

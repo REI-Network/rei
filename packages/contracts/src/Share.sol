65dda5197cd5a8bb151cdca9471a69840aab6c97
samlior <samlior@foxmail.com>
2021-08-04 16:14:59 +0800

    chore: update solidity version to ^0.6.0

--- packages/contracts/src/Share.sol
+++ packages/contracts/src/Share.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-pragma solidity 0.6.2;
+pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./interfaces/IConfig.sol";
--- packages/contracts/src/StakeManager.sol
+++ packages/contracts/src/StakeManager.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-pragma solidity 0.6.2;
+pragma solidity ^0.6.0;
 pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";

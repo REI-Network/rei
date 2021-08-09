678ea9298cca223cbf45cabb81f68d1c4be7e471
sspin <826015751@qq.com>
2021-08-09 15:29:35 +0800

    chore: add test for core/txpool/journal

---
+++ packages/core/test/txpool/journal.test.ts
@@ -0,0 +1,68 @@
+import fs from 'fs';
+import path from 'path';
+import { Common } from '@gxchain2/common';
+import { Journal } from '../../src/txpool/journal';
+import { Transaction } from '@gxchain2/structure';
+import { expect } from 'chai';
+import { createBufferFunctionalMap, hexStringToBuffer } from '@gxchain2/utils';
+
+class testnode {
+  getCommon(num: number) {
+    return Common.createCommonByBlockNumber(0, 'gxc2-testnet');
+  }
+}
+
+describe('Journal', () => {
+  let testdir: string;
+  let journal: Journal;
+  let testdata: any;
+  const node = new testnode();
+  const testTransactions: Transaction[] = [];
+  before(async () => {
+    testdata = JSON.parse(fs.readFileSync(path.join(__dirname, '/test-data.json')).toString());
+    testdata.transactions.forEach((element) => {
+      testTransactions.push(Transaction.fromTxData(element));
+    });
+    testdir = path.join(__dirname, '/test-dir');
+    if (!fs.existsSync(testdir)) {
+      fs.mkdirSync(testdir, { recursive: true });
+    }
+    journal = new Journal(testdir, node as any);
+    testTransactions.forEach(async (trx) => {
+      await journal.insert(trx);
+    });
+  });
+
+  it('should load correctly', async () => {
+    await journal.load(async (trxs: Transaction[]) => {
+      trxs.forEach((trx, i) => {
+        expect(trx.serialize().equals(testTransactions[i].serialize()), 'transaction should be equal').be.true;
+      });
+    });
+  });
+
+  it('should insert correctly', async () => {
+    const another = Transaction.fromTxData(testdata.another);
+    await journal.insert(another);
+    await journal.load(async (trxs: Transaction[]) => {
+      expect(trxs[4].serialize().equals(another.serialize()), 'transaction should be equal').be.true;
+    });
+  });
+
+  it('should rotate correctly', async () => {
+    const addr = hexStringToBuffer('0x2dd3cf3116858021c7a234ff470b21a8d3e547d4');
+    const remap = createBufferFunctionalMap<Transaction[]>();
+    remap.set(addr, testTransactions);
+    await journal.rotate(remap);
+    await journal.load(async (trxs: Transaction[]) => {
+      trxs.forEach((trx, i) => {
+        expect(trx.serialize().equals(testTransactions[i].serialize()), 'transaction should be equal').be.true;
+      });
+      expect(trxs.length, 'transaction should be rotated').be.equal(4);
+    });
+  });
+
+  after(async () => {
+    fs.rmdirSync(testdir, { recursive: true });
+  });
+});

c8cdb7cbca7f485cf2b340bc97e9a0c38376668c
samlior <samlior@foxmail.com>
2021-11-05 11:59:09 +0800

    feat: add file group for state machine

---
+++ packages/core/src/consensus/reimint/wal/fileGroup.ts
@@ -0,0 +1,407 @@
+import * as fs from 'fs/promises';
+import path from 'path';
+import Semaphore from 'semaphore-async-await';
+import { logger } from '@gxchain2/utils';
+
+const defaultGroupCheckDuration = 5 * 1000;
+const defaultHeadSizeLimit = 10 * 1024 * 1024;
+const defaultTotalSizeLimit = 1 * 1024 * 1024 * 1024;
+const defaultMaxFilesToRemove = 4;
+
+/**
+ * Make group file path by index,
+ * if index === maxIndex, return head path
+ * @param _path - Path to group file dir
+ * @param base - Group file base prefix
+ * @param index - Group file index
+ * @param maxIndex - Group file max index
+ * @returns Group file path
+ */
+export function makeGroupFilePath(_path: string, base: string, index: number, maxIndex?: number) {
+  if (maxIndex !== undefined && index === maxIndex) {
+    return path.join(_path, base);
+  }
+  return path.join(_path, `${base}.${String(index).padStart(3, '0')}`);
+}
+
+export interface FileGroupOptions {
+  // group file base prefix
+  base: string;
+  // path to group file dir
+  path: string;
+  // check duration, in each loop,
+  // FileGroup will check head size limit
+  // and total size limit
+  groupCheckDuration?: number;
+  // head size limit,
+  // if the head size is greater than this limit,
+  // FileGroup will rotate the head to the disk
+  headSizeLimit?: number;
+  // total size limit,
+  // if the total file size is greater than this limit,
+  // FileGroup will delete file from back to front
+  totalSizeLimit?: number;
+  // maximum number of files to be removed in each loop
+  maxFilesToRemove?: number;
+}
+
+export class FileGroup {
+  readonly base: string;
+  readonly path: string;
+  private groupCheckDuration: number;
+  private headSizeLimit: number;
+  private totalSizeLimit: number;
+  private maxFilesToRemove: number;
+  private _minIndex: number = 0;
+  private _maxIndex: number = 0;
+  private lock = new Semaphore(1);
+  private head?: fs.FileHandle;
+  private timeout?: NodeJS.Timeout;
+
+  constructor(options: FileGroupOptions) {
+    this.base = options.base;
+    this.path = options.path;
+    this.groupCheckDuration = options.groupCheckDuration ?? defaultGroupCheckDuration;
+    this.headSizeLimit = options.headSizeLimit ?? defaultHeadSizeLimit;
+    this.totalSizeLimit = options.totalSizeLimit ?? defaultTotalSizeLimit;
+    this.maxFilesToRemove = options.maxFilesToRemove ?? defaultMaxFilesToRemove;
+  }
+
+  /**
+   * Get current min index
+   */
+  get minIndex() {
+    this.requireHead();
+    return this._minIndex;
+  }
+
+  /**
+   * Get current max index
+   */
+  get maxIndex() {
+    this.requireHead();
+    return this._maxIndex;
+  }
+
+  private async runWithLock<T>(fn: () => Promise<T>) {
+    try {
+      await this.lock.acquire();
+      return await fn();
+    } catch (err) {
+      throw err;
+    } finally {
+      this.lock.release();
+    }
+  }
+
+  private requireHead() {
+    if (!this.head) {
+      throw new Error("head doesn't exsit");
+    }
+  }
+
+  /**
+   * Open file group
+   */
+  open() {
+    return this.runWithLock(async () => {
+      if (this.head) {
+        throw new Error('head already exists');
+      }
+      const { minIndex, maxIndex } = await this.readGroupInfo();
+      this._minIndex = minIndex;
+      this._maxIndex = maxIndex;
+      this.head = await fs.open(makeGroupFilePath(this.path, this.base, 0, 0), 'a');
+      this.setTimeout();
+    });
+  }
+
+  /**
+   * Close file group
+   */
+  close() {
+    return this.runWithLock(async () => {
+      if (this.timeout) {
+        clearTimeout(this.timeout);
+        this.timeout = undefined;
+      }
+      if (this.head) {
+        await this.head?.close();
+        this.head = undefined;
+      }
+    });
+  }
+
+  /**
+   * Write data to file group
+   * @param data - Data
+   * @param flush - Whether to flush to disk
+   */
+  write(data: Buffer, flush?: boolean) {
+    return this.runWithLock(async () => {
+      this.requireHead();
+      await this.head!.write(data);
+      if (flush) {
+        await this.head!.sync();
+      }
+    });
+  }
+
+  /**
+   * Flush to disk
+   */
+  flush() {
+    return this.runWithLock(async () => {
+      this.requireHead();
+      await this.head!.sync();
+    });
+  }
+
+  /**
+   * Create a new group file reader
+   * @returns Group file reader
+   */
+  newReader() {
+    return new GroupFileReader(this, this.minIndex);
+  }
+
+  /**
+   * Read group info from file group dir
+   * NOTE: minIndex and maxIndex may be different from `fileGroup.minIndex` and `fileGroup.maxIndex`
+   * @returns Group info
+   */
+  async readGroupInfo() {
+    const files = await fs.readdir(this.path);
+    let minIndex = -1;
+    let maxIndex = -1;
+    let totalSize = 0;
+    let headSize = 0;
+
+    for (const file of files) {
+      if (file === this.base) {
+        // header
+        const stats = await fs.stat(makeGroupFilePath(this.path, this.base, 0, 0));
+        totalSize += stats.size;
+        headSize = stats.size;
+      } else if (file.startsWith(this.base)) {
+        // flushed files
+        const execArray = /^.+\.([0-9]{3,})$/.exec(file);
+        if (execArray !== null && execArray.length === 2) {
+          const strIndex = execArray[1];
+          const index = Number(strIndex);
+          if (!Number.isInteger(index)) {
+            continue;
+          }
+          const _path = makeGroupFilePath(this.path, this.base, index);
+          // check file name
+          if (_path !== path.join(this.path, file)) {
+            continue;
+          }
+          const stats = await fs.stat(_path);
+          totalSize += stats.size;
+          if (index > maxIndex) {
+            maxIndex = index;
+          }
+          if (minIndex === -1 || index < minIndex) {
+            minIndex = index;
+          }
+        }
+      }
+    }
+
+    if (minIndex === -1) {
+      minIndex = 0;
+      maxIndex = 0;
+    } else {
+      // for head file
+      maxIndex++;
+    }
+
+    return {
+      minIndex,
+      maxIndex,
+      totalSize,
+      headSize
+    };
+  }
+
+  private setTimeout() {
+    this.timeout = setTimeout(() => {
+      this.timeout = undefined;
+      this.tryToCheck()
+        .then(() => {
+          this.runWithLock(async () => {
+            if (this.head) {
+              this.setTimeout();
+            }
+          });
+        })
+        .catch((err) => {
+          logger.error('FileGroup::setTimeout, catch error:', err);
+          if (this.head) {
+            this.runWithLock(async () => {
+              if (this.head) {
+                this.setTimeout();
+              }
+            });
+          }
+        });
+    }, this.groupCheckDuration);
+  }
+
+  private tryToCheck() {
+    return this.runWithLock(async () => {
+      if (this.head) {
+        await this.checkHeadSizeLimit();
+        await this.checkTotalSizeLimit();
+      }
+    });
+  }
+
+  private async checkHeadSizeLimit() {
+    if (this.headSizeLimit === 0) {
+      return;
+    }
+
+    const headSize = (await this.head!.stat()).size;
+    if (headSize > this.headSizeLimit) {
+      // rotate head
+      await this.head!.sync();
+      await this.head!.close();
+      const oldPath = makeGroupFilePath(this.path, this.base, 0, 0);
+      const newPath = makeGroupFilePath(this.path, this.base, this._maxIndex, this._maxIndex + 1);
+      await fs.rename(oldPath, newPath);
+      this._maxIndex++;
+      this.head = await fs.open(oldPath, 'a');
+    }
+  }
+
+  private async checkTotalSizeLimit() {
+    if (this.totalSizeLimit === 0) {
+      return;
+    }
+
+    let { totalSize, minIndex, maxIndex } = await this.readGroupInfo();
+    if (totalSize > this.totalSizeLimit) {
+      for (let i = 0; i < this.maxFilesToRemove; i++) {
+        const index = minIndex + i;
+        if (index === maxIndex) {
+          return;
+        }
+        const _path = makeGroupFilePath(this.path, this.base, index);
+        const stats = await fs.stat(_path);
+        await fs.rm(_path, { recursive: true, force: true });
+
+        totalSize -= stats.size;
+        if (totalSize <= this.totalSizeLimit) {
+          return;
+        }
+      }
+    }
+  }
+}
+
+export class GroupFileReader {
+  private index: number = 0;
+  private group: FileGroup;
+  private lock = new Semaphore(1);
+  private file?: fs.FileHandle;
+
+  constructor(group: FileGroup, index: number) {
+    this.group = group;
+    this.index = index;
+  }
+
+  private async runWithLock<T>(fn: () => Promise<T>) {
+    try {
+      await this.lock.acquire();
+      return await fn();
+    } catch (err) {
+      throw err;
+    } finally {
+      this.lock.release();
+    }
+  }
+
+  private async tryOpenFile(index: number) {
+    try {
+      return await fs.open(makeGroupFilePath(this.group.path, this.group.base, index, this.group.maxIndex), 'r');
+    } catch (err: any) {
+      if (err.code !== 'ENOENT') {
+        throw err;
+      }
+    }
+  }
+
+  private async openFile() {
+    let index = this.index;
+    while (!this.file && index <= this.group.maxIndex) {
+      this.file = await this.tryOpenFile((this.index = index++));
+    }
+    return !!this.file;
+  }
+
+  /**
+   * Close reader
+   */
+  close() {
+    return this.runWithLock(async () => {
+      if (this.file) {
+        await this.file.close();
+        this.file = undefined;
+      }
+    });
+  }
+
+  /**
+   * Read data from group file
+   * @param buf - The buffer that the data will be written to
+   * @returns If the file data cannot fill the entire buffer, return false,
+   *          otherwise, return true
+   */
+  read(buf: Buffer) {
+    return this.runWithLock(async () => {
+      let readLength = buf.length;
+      while (readLength > 0) {
+        if (!this.file && !(await this.openFile())) {
+          return false;
+        }
+
+        const { bytesRead } = await this.file!.read(buf, buf.length - readLength, readLength);
+        readLength -= bytesRead;
+
+        if (readLength > 0) {
+          await this.file!.close();
+          this.file = undefined;
+          if (this.index + 1 > this.group.maxIndex) {
+            return false;
+          }
+          this.index++;
+        }
+      }
+      return true;
+    });
+  }
+
+  /**
+   * Get current group file index
+   * @returns Group file index
+   */
+  getIndex() {
+    return this.runWithLock(() => Promise.resolve(this.index));
+  }
+
+  /**
+   * Set current group file index
+   * @param index - Index
+   */
+  setIndex(index: number) {
+    return this.runWithLock(async () => {
+      this.index = index;
+      if (this.file) {
+        await this.file.close();
+        this.file = undefined;
+      }
+    });
+  }
+}
---
+++ packages/core/test/fileGroup/fileGroup.test.ts
@@ -0,0 +1,252 @@
+import * as fs from 'fs/promises';
+import path from 'path';
+import crypto from 'crypto';
+import { expect } from 'chai';
+import { FileGroup, GroupFileReader, makeGroupFilePath } from '../../src/consensus/reimint/wal/fileGroup';
+
+class MockFileGroup {
+  readonly path: string;
+  readonly base: string;
+  readonly maxIndex: number;
+
+  constructor(path: string, base: string, maxIndex: number) {
+    this.path = path;
+    this.base = base;
+    this.maxIndex = maxIndex;
+  }
+}
+
+class MockBytes {
+  private content: string = '';
+
+  append(str: string) {
+    this.content += str;
+  }
+
+  read(length: number) {
+    const result = this.content.substr(0, length);
+    this.content = this.content.substr(length);
+    return result;
+  }
+}
+
+const testDir = path.join(__dirname, 'test-dir');
+const testBase = 'wal';
+const testMaxIndex = 10;
+const bytes = new MockBytes();
+
+const clearup = async () => {
+  try {
+    await fs.access(testDir);
+    await fs.rm(testDir, { recursive: true, force: true });
+  } catch (err) {
+    // ignore all errors
+  }
+};
+const writeData = async (index: number) => {
+  const content = `${index}|0123456789`;
+  await fs.writeFile(makeGroupFilePath(testDir, testBase, index, testMaxIndex), content);
+  bytes.append(content);
+};
+
+describe('GroupFileReader', () => {
+  const reader = new GroupFileReader(new MockFileGroup(testDir, testBase, testMaxIndex) as any, 0);
+
+  before(async () => {
+    await clearup();
+    await fs.mkdir(testDir);
+    // create test data file, 0 and 3, 4, 5, ... 12
+    await writeData(0);
+    for (let i = 3; i < 10; i++) {
+      await writeData(i);
+    }
+  });
+
+  it('should read succeed(1)', async () => {
+    const buf = Buffer.alloc(5);
+    expect(await reader.read(buf), 'should read succeed').be.true;
+    expect(buf.toString(), 'buffer content should be correct').be.equal(bytes.read(5));
+  });
+
+  it('should read succeed(2)', async () => {
+    const buf = Buffer.alloc(5);
+    expect(await reader.read(buf), 'should read succeed').be.true;
+    expect(buf.toString(), 'buffer content should be correct').be.equal(bytes.read(5));
+  });
+
+  it('should read succeed(3)', async () => {
+    const buf = Buffer.alloc(5);
+    expect(await reader.read(buf), 'should read succeed').be.true;
+    expect(buf.toString(), 'buffer content should be correct').be.equal(bytes.read(5));
+  });
+
+  it('should read succeed(4)', async () => {
+    const buf = Buffer.alloc(24);
+    expect(await reader.read(buf), 'should read succeed').be.true;
+    expect(buf.toString(), 'buffer content should be correct').be.equal(bytes.read(24));
+  });
+
+  it('should read succeed(5)', async () => {
+    const buf = Buffer.alloc(57);
+    expect(await reader.read(buf), 'should read succeed').be.true;
+    expect(buf.toString(), 'buffer content should be correct').be.equal(bytes.read(57));
+  });
+
+  it('should read faild', async () => {
+    const buf = Buffer.alloc(1);
+    expect(await reader.read(buf), 'should read succeed').be.false;
+  });
+
+  it('should close successfully', async () => {
+    await reader.close();
+  });
+
+  after(async () => {
+    await clearup();
+  });
+});
+
+describe('FileGroup', () => {
+  const groupCheckDuration = 300;
+  const fileGroup = new FileGroup({
+    path: testDir,
+    base: testBase,
+    groupCheckDuration,
+    headSizeLimit: 10,
+    totalSizeLimit: 100,
+    maxFilesToRemove: 2
+  });
+
+  before(async () => {
+    await clearup();
+    await fs.mkdir(testDir);
+    // create test data file, from 0 to 2
+    for (let i = 0; i < 3; i++) {
+      await writeData(i);
+    }
+  });
+
+  it('should read group info correctly', async () => {
+    const info = await fileGroup.readGroupInfo();
+    expect(info.minIndex, 'minIndex should be correct').be.equal(0);
+    expect(info.maxIndex, 'maxIndex should be correct').be.equal(3);
+    expect(info.totalSize, 'totalSize should be correct').be.equal(36);
+    expect(info.headSize, 'headSize should be correct').be.equal(0);
+  });
+
+  it('should open successfully', async () => {
+    await fileGroup.open();
+    expect(fileGroup.minIndex, 'minIndex should be correct').be.equal(0);
+    expect(fileGroup.maxIndex, 'maxIndex should be correct').be.equal(3);
+  });
+
+  it('should write successfully(1)', async () => {
+    await fileGroup.write(crypto.randomBytes(36), true);
+    const info = await fileGroup.readGroupInfo();
+    expect(info.minIndex, 'minIndex should be correct').be.equal(0);
+    expect(info.maxIndex, 'maxIndex should be correct').be.equal(3);
+    expect(info.totalSize, 'totalSize should be correct').be.equal(72);
+    expect(info.headSize, 'headSize should be correct').be.equal(36);
+  });
+
+  it('should rotate successfully', async () => {
+    await new Promise((r) => setTimeout(r, groupCheckDuration + 10));
+    const info = await fileGroup.readGroupInfo();
+    expect(info.minIndex, 'minIndex should be correct').be.equal(0);
+    expect(info.maxIndex, 'maxIndex should be correct').be.equal(4);
+    expect(info.totalSize, 'totalSize should be correct').be.equal(72);
+    expect(info.headSize, 'headSize should be correct').be.equal(0);
+  });
+
+  it('should write successfully(2)', async () => {
+    await fileGroup.write(crypto.randomBytes(9), true);
+    const info = await fileGroup.readGroupInfo();
+    expect(info.minIndex, 'minIndex should be correct').be.equal(0);
+    expect(info.maxIndex, 'maxIndex should be correct').be.equal(4);
+    expect(info.totalSize, 'totalSize should be correct').be.equal(81);
+    expect(info.headSize, 'headSize should be correct').be.equal(9);
+  });
+
+  it("shouldn't rotate", async () => {
+    await new Promise((r) => setTimeout(r, groupCheckDuration + 10));
+    const info = await fileGroup.readGroupInfo();
+    expect(info.minIndex, 'minIndex should be correct').be.equal(0);
+    expect(info.maxIndex, 'maxIndex should be correct').be.equal(4);
+    expect(info.totalSize, 'totalSize should be correct').be.equal(81);
+    expect(info.headSize, 'headSize should be correct').be.equal(9);
+  });
+
+  it('should write successfully(3)', async () => {
+    await fileGroup.write(crypto.randomBytes(100), true);
+    const info = await fileGroup.readGroupInfo();
+    expect(info.minIndex, 'minIndex should be correct').be.equal(0);
+    expect(info.maxIndex, 'maxIndex should be correct').be.equal(4);
+    expect(info.totalSize, 'totalSize should be correct').be.equal(181);
+    expect(info.headSize, 'headSize should be correct').be.equal(109);
+  });
+
+  it('should remove successfully(1)', async () => {
+    // current:
+    // index: 4, size: 109 bytes
+    // index: 3, size: 36 bytes
+    // index: 2, size: 12 bytes
+    // index: 1, size: 12 bytes
+    // index: 0, size: 12 bytes
+    await new Promise((r) => setTimeout(r, groupCheckDuration + 10));
+    // current:
+    // index: 4, size: 109 bytes
+    // index: 3, size: 36 bytes
+    // index: 2, size: 12 bytes
+    //
+    // because total size is greater than totalSizeLimit,
+    // but maxFilesToRemove = 2
+    const info = await fileGroup.readGroupInfo();
+    expect(fileGroup.minIndex, 'minIndex should be correct').be.equal(0);
+    expect(fileGroup.maxIndex, 'maxIndex should be correct').be.equal(5);
+    expect(info.minIndex, 'minIndex should be correct').be.equal(2);
+    expect(info.maxIndex, 'maxIndex should be correct').be.equal(5);
+    expect(info.totalSize, 'totalSize should be correct').be.equal(157);
+    expect(info.headSize, 'headSize should be correct').be.equal(0);
+  });
+
+  it('should remove successfully(2)', async () => {
+    // current:
+    // index: 4, size: 109 bytes
+    // index: 3, size: 36 bytes
+    // index: 2, size: 12 bytes
+    await new Promise((r) => setTimeout(r, groupCheckDuration + 10));
+    // current:
+    // index: 4, size: 109 bytes
+    // same reason as above
+    const info = await fileGroup.readGroupInfo();
+    expect(fileGroup.minIndex, 'minIndex should be correct').be.equal(0);
+    expect(fileGroup.maxIndex, 'maxIndex should be correct').be.equal(5);
+    expect(info.minIndex, 'minIndex should be correct').be.equal(4);
+    expect(info.maxIndex, 'maxIndex should be correct').be.equal(5);
+    expect(info.totalSize, 'totalSize should be correct').be.equal(109);
+    expect(info.headSize, 'headSize should be correct').be.equal(0);
+  });
+
+  it('should remove successfully(2)', async () => {
+    // current:
+    // index: 4, size: 109 bytes
+    await new Promise((r) => setTimeout(r, groupCheckDuration + 10));
+    // current:
+    // same reason as above
+    const info = await fileGroup.readGroupInfo();
+    expect(fileGroup.minIndex, 'minIndex should be correct').be.equal(0);
+    expect(fileGroup.maxIndex, 'maxIndex should be correct').be.equal(5);
+    expect(info.minIndex, 'minIndex should be correct').be.equal(0);
+    expect(info.maxIndex, 'maxIndex should be correct').be.equal(0);
+    expect(info.totalSize, 'totalSize should be correct').be.equal(0);
+    expect(info.headSize, 'headSize should be correct').be.equal(0);
+  });
+
+  it('should close successfully', async () => {
+    await fileGroup.close();
+  });
+
+  after(async () => {
+    await clearup();
+  });
+});

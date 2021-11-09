cab2479c18f7e5a60c997f3a4f8234418739346b
samlior <samlior@foxmail.com>
2021-11-09 11:20:30 +0800

    chore: add test and improve db

--- packages/core/src/consensus/reimint/types/evdb.ts
+++ packages/core/src/consensus/reimint/types/evdb.ts
@@ -1,27 +1,56 @@
 import { LevelUp } from 'levelup';
 import { AbstractIterator } from 'abstract-leveldown';
-import { intToBuffer, toBuffer, BN } from 'ethereumjs-util';
+import { intToBuffer, BN } from 'ethereumjs-util';
 import { Evidence, EvidenceFactory } from './evidence';
 import { EMPTY_HASH } from '../../../utils';
 
 const PENDING_PREFIX = 1;
 const COMMITED_PREFIX = 2;
 const MAX_HASH = Buffer.from(new Uint8Array(32).fill(0xff));
+const MAX_UINT64 = new BN('ffffffffffffffff', 'hex');
 
+/**
+ * Append an empty key for pending evidence
+ * @param height - Target height
+ * @returns Pending key
+ */
 function emptyKeyPending(height: BN) {
-  return Buffer.concat([intToBuffer(PENDING_PREFIX), toBuffer(height), EMPTY_HASH]);
+  const heightBuffer = Buffer.alloc(8);
+  heightBuffer.writeBigUInt64BE(BigInt(height.toString()));
+  return Buffer.concat([intToBuffer(PENDING_PREFIX), heightBuffer, EMPTY_HASH]);
 }
 
+/**
+ * Append a max key for pending evidence
+ * @param height - Target height
+ * @returns Pending key
+ */
 function maxKeyPending(height: BN) {
-  return Buffer.concat([intToBuffer(PENDING_PREFIX), toBuffer(height), MAX_HASH]);
+  const heightBuffer = Buffer.alloc(8);
+  heightBuffer.writeBigUInt64BE(BigInt(height.toString()));
+  return Buffer.concat([intToBuffer(PENDING_PREFIX), heightBuffer, MAX_HASH]);
 }
 
+/**
+ * Append a key for pending evidence
+ * @param ev - Evidence
+ * @returns Pending key
+ */
 function keyPending(ev: Evidence) {
-  return Buffer.concat([intToBuffer(PENDING_PREFIX), toBuffer(ev.height), ev.hash()]);
+  const heightBuffer = Buffer.alloc(8);
+  heightBuffer.writeBigUInt64BE(BigInt(ev.height.toString()));
+  return Buffer.concat([intToBuffer(PENDING_PREFIX), heightBuffer, ev.hash()]);
 }
 
+/**
+ * Append a key for committed evidence
+ * @param ev - Evidence
+ * @returns Committed key
+ */
 function keyCommitted(ev: Evidence) {
-  return Buffer.concat([intToBuffer(COMMITED_PREFIX), toBuffer(ev.height), ev.hash()]);
+  const heightBuffer = Buffer.alloc(8);
+  heightBuffer.writeBigUInt64BE(BigInt(ev.height.toString()));
+  return Buffer.concat([intToBuffer(COMMITED_PREFIX), heightBuffer, ev.hash()]);
 }
 
 export class EvidenceDatabase {
@@ -31,6 +60,11 @@ export class EvidenceDatabase {
     this.db = db;
   }
 
+  /**
+   * Check whether the evidence is pending
+   * @param ev - Evidence
+   * @returns Return true if it is pending
+   */
   async isPending(ev: Evidence) {
     try {
       await this.db.get(keyPending(ev));
@@ -43,6 +77,11 @@ export class EvidenceDatabase {
     }
   }
 
+  /**
+   * Check whether the evidence is committed
+   * @param ev - Evidence
+   * @returns Return true if it is committed
+   */
   async isCommitted(ev: Evidence) {
     try {
       await this.db.get(keyCommitted(ev));
@@ -55,14 +94,26 @@ export class EvidenceDatabase {
     }
   }
 
+  /**
+   * Add a pending evidence to database
+   * @param ev - Evidence
+   */
   addPendingEvidence(ev: Evidence) {
     return this.db.put(keyPending(ev), ev.serialize());
   }
 
+  /**
+   * Remove a pending evidence from database
+   * @param ev - Evidence
+   */
   removePendingEvidence(ev: Evidence) {
     return this.db.del(keyPending(ev), ev.serialize());
   }
 
+  /**
+   * Add a committed evidence to database
+   * @param ev - Evidence
+   */
   addCommittedEvidence(ev: Evidence) {
     return this.db.put(keyCommitted(ev), ev.serialize());
   }
@@ -74,10 +125,10 @@ export class EvidenceDatabase {
    * @param reverse - Set to true if you want the stream to go in reverse order
    * @param onData - Data callback, return true to break the search loop
    */
-  async loadPendingEvidence({ from, to, reverse, onData }: { from?: BN; to?: BN; reverse?: boolean; onData: (data: Evidence) => boolean }) {
+  async loadPendingEvidence({ from, to, reverse, onData }: { from?: BN; to?: BN; reverse?: boolean; onData: (data: Evidence) => Promise<boolean> }) {
     const itr: AbstractIterator<Buffer, Buffer> = this.db.iterator({
-      gte: from ? emptyKeyPending(from) : undefined,
-      lte: to ? maxKeyPending(to) : undefined,
+      gte: emptyKeyPending(from ?? new BN(0)),
+      lte: maxKeyPending(to ?? MAX_UINT64),
       reverse
     });
     const next = () => {
@@ -105,7 +156,7 @@ export class EvidenceDatabase {
 
     try {
       let serialized: Buffer | undefined;
-      while ((serialized = await next()) && !onData(EvidenceFactory.fromSerializedEvidence(serialized))) {}
+      while ((serialized = await next()) && !(await onData(EvidenceFactory.fromSerializedEvidence(serialized)))) {}
     } catch (err) {
       throw err;
     } finally {
---
+++ packages/core/test/evpool/evdb.test.ts
@@ -0,0 +1,104 @@
+import * as fs from 'fs/promises';
+import path from 'path';
+import { expect } from 'chai';
+import { BN } from 'ethereumjs-util';
+import { LevelUp } from 'levelup';
+import { createLevelDB } from '@gxchain2/database';
+import { EvidenceDatabase, MockEvidence, Evidence } from '../../src/consensus/reimint/types';
+
+describe('EvidenceDatabase', () => {
+  const testDir = path.join(__dirname, 'test-dir');
+  let rawdb: LevelUp;
+  let db: EvidenceDatabase;
+
+  const clearup = async () => {
+    try {
+      await fs.access(testDir);
+      await fs.rm(testDir, { recursive: true, force: true });
+    } catch (err) {
+      // ignore all errors
+    }
+  };
+
+  before(async () => {
+    await clearup();
+    rawdb = createLevelDB(testDir);
+    db = new EvidenceDatabase(rawdb);
+  });
+
+  it('should write successfully(1)', async () => {
+    for (let i = 0; i < 5; i++) {
+      await db.addPendingEvidence(new MockEvidence(new BN(100 + i)));
+    }
+  });
+
+  it('should load and remove successfully', async () => {
+    const evList: Evidence[] = [];
+    await db.loadPendingEvidence({
+      from: new BN(0),
+      onData: async (ev) => {
+        evList.push(ev);
+        await db.removePendingEvidence(ev);
+        return false;
+      }
+    });
+
+    expect(evList.length).be.equal(5);
+    for (let i = 0; i < 5; i++) {
+      expect(evList[i].height.toNumber()).be.equal(100 + i);
+    }
+  });
+
+  it('should load successfully', async () => {
+    const evList: Evidence[] = [];
+    await db.loadPendingEvidence({
+      from: new BN(0),
+      onData: async (ev) => {
+        evList.push(ev);
+        return false;
+      }
+    });
+
+    expect(evList.length).be.equal(0);
+  });
+
+  const uint64Max = new BN('ffffffffffffffff', 'hex');
+  it('should write successfully(2)', async () => {
+    await db.addPendingEvidence(new MockEvidence(new BN(1111112345)));
+    await db.addPendingEvidence(new MockEvidence(new BN(0)));
+    await db.addPendingEvidence(new MockEvidence(new BN(12345)));
+    await db.addPendingEvidence(new MockEvidence(uint64Max));
+
+    await db.addCommittedEvidence(new MockEvidence(new BN(11231231)));
+  });
+
+  it('should load successfully(reverse)', async () => {
+    const evList: Evidence[] = [];
+    await db.loadPendingEvidence({
+      to: uint64Max,
+      from: new BN(0),
+      reverse: true,
+      onData: async (ev) => {
+        evList.push(ev);
+        return false;
+      }
+    });
+
+    expect(evList.length).be.equal(4);
+    expect(evList[0].height.toString()).be.equal(uint64Max.toString());
+    expect(evList[1].height.toNumber()).be.equal(1111112345);
+    expect(evList[2].height.toNumber()).be.equal(12345);
+    expect(evList[3].height.toNumber()).be.equal(0);
+  });
+
+  it('should check successfully', async () => {
+    expect(await db.isPending(new MockEvidence(new BN(0)))).be.true;
+    expect(await db.isPending(new MockEvidence(new BN(11231231)))).be.false;
+    expect(await db.isCommitted(new MockEvidence(new BN(11231231)))).be.true;
+  });
+
+  after(async () => {
+    await rawdb.close();
+    await clearup();
+  });
+});

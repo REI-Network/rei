32de2e71439b9f7838d3d3945556d0e167298a4b
samlior <samlior@foxmail.com>
2021-11-23 14:15:19 +0800

    chore: add test for fee

--- packages/test/src/client.ts
+++ packages/test/src/client.ts
@@ -1,11 +1,20 @@
+import crypto from 'crypto';
 import path from 'path';
-import { BN } from 'ethereumjs-util';
+import { BN, Address, bufferToHex } from 'ethereumjs-util';
 import Web3 from 'web3';
 import { Contract } from 'web3-eth-contract';
+import { MockAccountManager } from '../../core/test/util';
+
+export type TxOptions = {
+  from?: string;
+  to?: string;
+  value?: string;
+};
 
 export class Client {
   readonly web3: Web3;
   readonly configAddress: string;
+  readonly accMngr: MockAccountManager;
   config!: Contract;
   fee!: Contract;
   freeFee!: Contract;
@@ -16,9 +25,10 @@ export class Client {
   unstakePool!: Contract;
   validatorRewardPool!: Contract;
 
-  constructor(provider: string, configAddress: string) {
+  constructor(provider = 'ws://127.0.0.1:11451', configAddress = '0x0000000000000000000000000000000000001000') {
     this.web3 = new Web3(provider);
     this.configAddress = configAddress;
+    this.accMngr = new MockAccountManager([]);
   }
 
   private loadABI(contract: string, file?: string) {
@@ -40,6 +50,27 @@ export class Client {
     await initContract('stakeManager');
     await initContract('unstakePool');
     await initContract('validatorRewardPool');
+
+    // add accounts
+    this.accMngr.add([
+      ['genesis1', Address.fromString('0xff96a3bff24da3d686fea7bd4beb5ccfd7868dde'), Buffer.from('225a70405aa06a0dc0451fb51a9284a0dab949257f8a2df90192b5238e76936a', 'hex')],
+      ['admin', Address.fromString('0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266'), Buffer.from('ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80', 'hex')]
+    ]);
+
+    // create random accounts
+    for (let i = 0; i < 3; i++) {
+      const priv = crypto.randomBytes(32);
+      const addr = Address.fromPrivateKey(priv);
+      this.accMngr.add([[`test${i}`, addr, priv]]);
+    }
+
+    // unlock all accounts
+    for (const [name, addr] of this.accMngr.nameToAddress) {
+      this.web3.eth.accounts.wallet.add({
+        address: addr.toString(),
+        privateKey: bufferToHex(this.accMngr.n2p(name))
+      });
+    }
   }
 
   parseUsageInfoLog(log: any) {
@@ -56,5 +87,15 @@ export class Client {
     const contractFeeUsage = new BN(data.substr(index++ * 64, 64), 'hex');
     const balanceUsage = new BN(data.substr(index++ * 64, 64), 'hex');
     return { feeUsage, freeFeeUsage, contractFeeUsage, balanceUsage };
+  }
+
+  sendTestTransaction(gasPrice: BN, options?: TxOptions) {
+    return this.web3.eth.sendTransaction({
+      from: this.accMngr.n2a(options?.from ?? 'test1').toString(),
+      to: this.accMngr.n2a(options?.to ?? 'genesis1').toString(),
+      value: options?.value ?? 0,
+      gas: 21000,
+      gasPrice: gasPrice.toString()
+    });
   }
 }
--- packages/test/test/freeFee.test.ts
+++ packages/test/test/freeFee.test.ts
@@ -1,64 +1,29 @@
-import crypto from 'crypto';
-import { Address, bufferToHex, BN } from 'ethereumjs-util';
-import { expect, assert } from 'chai';
-import { MockAccountManager } from '../../core/test/util';
+import { BN } from 'ethereumjs-util';
+import { expect } from 'chai';
 import { Client } from '../src';
 
-const accMngr = new MockAccountManager([
-  ['genesis1', Address.fromString('0xff96a3bff24da3d686fea7bd4beb5ccfd7868dde'), Buffer.from('225a70405aa06a0dc0451fb51a9284a0dab949257f8a2df90192b5238e76936a', 'hex')],
-  ['admin', Address.fromString('0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266'), Buffer.from('ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80', 'hex')]
-]);
-
-const client = new Client('ws://127.0.0.1:11451', '0x0000000000000000000000000000000000001000');
+const client = new Client();
+const { accMngr, web3 } = client;
 
 let userFreeFeeLimit!: BN;
-let freeFeeRecoverInterval!: number;
-
-const sendTestTransaction = (gasPrice: BN) => {
-  return client.web3.eth.sendTransaction({
-    from: accMngr.n2a('test1').toString(),
-    to: accMngr.n2a('genesis1').toString(),
-    value: 0,
-    gas: 21000,
-    gasPrice: gasPrice.toString()
-  });
-};
 
 describe('Free fee', () => {
   before(async () => {
     await client.init();
 
-    // create a random account
-    const priv = crypto.randomBytes(32);
-    const addr = Address.fromPrivateKey(priv);
-    accMngr.add([['test1', addr, priv]]);
-
-    // unlock all accounts
-    for (const [name, addr] of accMngr.nameToAddress) {
-      client.web3.eth.accounts.wallet.add({
-        address: addr.toString(),
-        privateKey: bufferToHex(accMngr.n2p(name))
-      });
-    }
-
     // transfer amount to the test1 account
     const amount = '1' + '0'.repeat(18); // 1 GXC
-    await client.web3.eth.sendTransaction({
-      from: accMngr.n2a('genesis1').toString(),
-      to: accMngr.n2a('test1').toString(),
-      value: amount,
-      gas: 21000
+    await client.sendTestTransaction(new BN(1), {
+      from: 'genesis1',
+      to: 'test1',
+      value: amount
     });
 
     // ensure user balance
-    expect(await client.web3.eth.getBalance(accMngr.n2a('test1').toString())).be.equal(amount);
+    expect(await web3.eth.getBalance(accMngr.n2a('test1').toString())).be.equal(amount);
 
     // load config
     userFreeFeeLimit = new BN(await client.config.methods.userFreeFeeLimit().call());
-    freeFeeRecoverInterval = Number(await client.config.methods.freeFeeRecoverInterval().call());
-    if (!Number.isInteger(freeFeeRecoverInterval)) {
-      assert.fail('invalid freeFeeRecoverInterval');
-    }
 
     // update free fee recover interval to 1 day
     await client.config.methods.setFreeFeeRecoverInterval(86400).send({ from: accMngr.n2a('admin').toString(), gas: 100000, gasPrice: 1 });
@@ -68,7 +33,7 @@ describe('Free fee', () => {
     expect((await client.freeFee.methods.userUsage(accMngr.n2a('test1').toString()).call()).usage).be.equal('0');
 
     const gasPrice = new BN(10);
-    const { logs } = await sendTestTransaction(gasPrice);
+    const { logs } = await client.sendTestTransaction(gasPrice);
 
     expect(logs.length > 0).be.true;
     const { feeUsage, freeFeeUsage, contractFeeUsage, balanceUsage } = client.parseUsageInfoLog(logs[logs.length - 1]);
@@ -86,7 +51,7 @@ describe('Free fee', () => {
     const gasUsed = gasPrice.muln(21000);
     const freeFeeUsed = left.clone();
     const balanceUsed = gasUsed.sub(freeFeeUsed);
-    const { logs } = await sendTestTransaction(gasPrice);
+    const { logs } = await client.sendTestTransaction(gasPrice);
 
     expect(logs.length > 0).be.true;
     const { feeUsage, freeFeeUsage, contractFeeUsage, balanceUsage } = client.parseUsageInfoLog(logs[logs.length - 1]);
@@ -99,7 +64,7 @@ describe('Free fee', () => {
 
   it('should transfer successfully(3)', async () => {
     const gasPrice = new BN(1);
-    const { logs } = await sendTestTransaction(gasPrice);
+    const { logs } = await client.sendTestTransaction(gasPrice);
 
     expect(logs.length > 0).be.true;
     const { feeUsage, freeFeeUsage, contractFeeUsage, balanceUsage } = client.parseUsageInfoLog(logs[logs.length - 1]);

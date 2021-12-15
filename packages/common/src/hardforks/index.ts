efe0e397ba27d6d244fa2011800ccd1e9ce2b95e
samlior <samlior@foxmail.com>
2021-12-15 15:13:31 +0800

    chore: update testnet and devnet genesis file

--- packages/common/src/hardforks/index.ts
+++ packages/common/src/hardforks/index.ts
@@ -1,6 +1,5 @@
 export const hardforks = [
   ['devnet-chainstart', require('./devnet-chainstart.json')],
   ['mainnet-chainstart', require('./mainnet-chainstart.json')],
-  ['testnet-chainstart', require('./testnet-chainstart.json')],
-  ['testnet-hf1', require('./testnet-hf1.json')]
+  ['testnet-chainstart', require('./testnet-chainstart.json')]
 ];

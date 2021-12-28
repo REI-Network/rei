f249e6cce8fa37018c82e621baa86201d84d839c
samlior <samlior@foxmail.com>
2021-12-28 15:05:20 +0800

    chore: add comments

--- packages/rpc/src/index.ts
+++ packages/rpc/src/index.ts
@@ -15,6 +15,7 @@ const defaultPort = 11451;
 const defaultHost = '127.0.0.1';
 const defaultApis = 'eth,net,web3';
 
+// long time-consuming requests that need to be queued for processing
 const queuedMethods = new Set<string>(['eth_call', 'eth_estimateGas', 'eth_getLogs']);
 
 export interface RpcServerOptions {

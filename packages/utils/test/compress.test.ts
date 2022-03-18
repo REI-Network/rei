b7943be60f076bfd7d49dabdd15752b06901724e
samlior <samlior@foxmail.com>
2022-03-18 15:55:04 +0800

    chore: fix compress test

--- packages/utils/test/compress.test.ts
+++ packages/utils/test/compress.test.ts
@@ -1,23 +1,23 @@
 import { expect } from 'chai';
 import { hexStringToBuffer, compressBytes, decompressBytes } from '../src';
 
-const testdata = ['0x0000000000000000000000000000000000000000000000000de0b6b3a7640000', '0x6dccc565ee3296e533e4be98733e284a45dz7ca72883935f418c11f284354a3c'];
-
 describe('Compress', () => {
-  it('should shorter than the original data', () => {
-    const compressed = compressBytes(hexStringToBuffer(testdata[0]));
-    expect(compressed.length, 'compressed should be shorter').be.lt(hexStringToBuffer(testdata[0]).length);
+  const data1 = hexStringToBuffer('0x0000000000000000000000000000000000000000000000000de0b6b3a7640000');
+  const data2 = hexStringToBuffer('0x6dccc565ee3296e533e4be98733e284a45da7ca72883935f418c11f284354a3c');
+
+  it('length should be shorter than the original data', () => {
+    const compressed = compressBytes(data1);
+    expect(compressed.length, 'compressed should be shorter').be.lt(data1.length);
   });
 
-  it('should equal raw data', () => {
-    const compressed = compressBytes(hexStringToBuffer(testdata[1]));
-    expect(compressed.equals(hexStringToBuffer(testdata[1])), 'should be equal').be.true;
+  it('length should be equal to the original data', () => {
+    const compressed = compressBytes(data2);
+    expect(compressed.equals(data2), 'should be equal').be.true;
   });
 
   it('should decompress correctly', () => {
-    const testbuffer = hexStringToBuffer(testdata[0]);
-    const compressed = compressBytes(testbuffer);
-    const decompressed = decompressBytes(compressed, testbuffer.length);
-    expect(decompressed.equals(testbuffer), 'the decompressed data needs to be equal to the original data').be.true;
+    const compressed = compressBytes(data1);
+    const decompressed = decompressBytes(compressed, data1.length);
+    expect(decompressed.equals(data1), 'the decompressed data should be equal').be.true;
   });
 });

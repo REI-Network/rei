5f3295e3ba8d73e8027b42dc18769fca47348108
samlior <samlior@foxmail.com>
2022-04-12 14:02:56 +0800

    chore: improve test case for trie sync

--- packages/core/test/snap/trieSync.test.ts
+++ packages/core/test/snap/trieSync.test.ts
@@ -1,10 +1,13 @@
 import { expect } from 'chai';
 import { LevelUp } from 'levelup';
-import { SecureTrie } from 'merkle-patricia-tree';
+import { SecureTrie, BaseTrie } from 'merkle-patricia-tree';
+import { BranchNode, TrieNode, ExtensionNode } from 'merkle-patricia-tree/dist/trieNode';
 import { Common } from '@rei-network/common';
 import { Database, DBOpData } from '@rei-network/database';
+import { getRandomIntInclusive } from '@rei-network/utils';
 import { RawDBatch } from '../../src/snap/batch';
 import { TrieSync, TrieSyncBackend } from '../../src/snap/trieSync';
+import { TrieIterator } from '../../src/snap/trieIterator';
 import { AccountInfo, genRandomAccounts } from './util';
 const level = require('level-mem');
 
@@ -14,7 +17,11 @@ common.setHardforkByBlockNumber(0);
 const rawDBOpts = { keyEncoding: 'binary', valueEncoding: 'binary' };
 
 class MockTrieSyncBackend implements TrieSyncBackend {
-  readonly rawdb: LevelUp = level();
+  readonly rawdb: LevelUp;
+
+  constructor(rawdb: LevelUp = level()) {
+    this.rawdb = rawdb;
+  }
 
   async batch(batch: DBOpData[]) {
     await this.rawdb.batch(batch as any);
@@ -51,14 +58,8 @@ describe('TrieSync', () => {
   let accounts!: AccountInfo[];
   let root!: Buffer;
 
-  before(async () => {
-    const result = await genRandomAccounts(db, 20, 20, false);
-    accounts = result.accounts;
-    root = result.root;
-  });
-
-  it('should sync trie succeed', async () => {
-    const backend = new MockTrieSyncBackend();
+  async function syncAndCheck(rawdb2?: LevelUp) {
+    const backend = new MockTrieSyncBackend(rawdb2);
 
     const sync = new TrieSync(backend);
     await sync.init(root);
@@ -92,5 +93,61 @@ describe('TrieSync', () => {
         expect(val2 && val2.equals(val), 'storage value should be equal').be.true;
       }
     }
+  }
+
+  before(async () => {
+    const result = await genRandomAccounts(db, 20, 20, false);
+    accounts = result.accounts;
+    root = result.root;
+  });
+
+  it("should sync trie succeed(when the whole tree doesn't exist)", async () => {
+    await syncAndCheck();
+  });
+
+  it("should sync trie succeed(when some nodes don't exist)", async () => {
+    const rawdb2: LevelUp = level();
+
+    // copy whole tree
+    for await (const { key, val } of new TrieIterator(new BaseTrie(rawdb, root))) {
+      await rawdb2.put(key, val, rawDBOpts);
+    }
+
+    // ensure the root node is a branch node
+    const trie = new BaseTrie(rawdb, root);
+    const node = await trie._lookupNode(root);
+    if (!(node instanceof BranchNode)) {
+      throw new Error('the root node is not a branch node, please run test cases again');
+    }
+
+    // randomly pick a child branch
+    const childNodes: (Buffer | Buffer[])[] = [];
+    for (let i = 0; i < 16; i++) {
+      const childNode = node.getBranch(i);
+      if (childNode && childNode.length > 0) {
+        childNodes.push(childNode);
+      }
+    }
+
+    // delete whole child branch
+    const deleteNode = async (node: TrieNode) => {
+      if (node instanceof BranchNode) {
+        for (let i = 0; i < 16; i++) {
+          const childNode = node.getBranch(i);
+          if (childNode && childNode.length > 0) {
+            const child = await trie._lookupNode(childNode);
+            await deleteNode(child!);
+          }
+        }
+      } else if (node instanceof ExtensionNode) {
+        const child = await trie._lookupNode(node._value);
+        await deleteNode(child!);
+      }
+
+      await rawdb2.del(node.hash(), rawDBOpts);
+    };
+    await deleteNode((await trie._lookupNode(childNodes[getRandomIntInclusive(0, childNodes.length - 1)]))!);
+
+    await syncAndCheck(rawdb2);
   });
 });

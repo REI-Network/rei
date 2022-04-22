87a41054a3e78bb1258e9a8e7c8606da64abb493
samlior <samlior@foxmail.com>
2022-04-22 10:13:10 +0800

    chore: add updateConfirmContext logic

--- packages/core/src/sync/sync.ts
+++ packages/core/src/sync/sync.ts
@@ -255,19 +255,136 @@ export class Sync {
 
   private pickRandomPeerToSync() {
     if (!this.isWorking && this.channel.array.length === 0) {
-      const handlers: WireProtocolHandler[] = [];
       const td = this.backend.getTotalDifficulty();
-      for (const handler of this.pool.handlers) {
-        if (new BN(handler.status!.totalDifficulty).gt(td)) {
-          handlers.push(handler);
-        }
-      }
-
+      const handlers = this.pool.handlers.filter((handler) => new BN(handler.status!.totalDifficulty).gt(td));
       if (handlers.length === 0) {
         return;
       }
 
       this.channel.push({ handler: handlers[getRandomIntInclusive(0, handlers.length - 1)] });
+    }
+  }
+
+  private async collectConfirm(context: ConfirmContext) {
+    const { confirmed, unconfirmed, height } = context;
+
+    const td = this.backend.getTotalDifficulty();
+    const handlers = this.pool.handlers.filter(({ peer: { peerId }, status }) => {
+      return new BN(status!.totalDifficulty).gt(td) && !confirmed.has(peerId) && !unconfirmed.has(peerId);
+    });
+
+    if (handlers.length === 0) {
+      return;
+    }
+
+    const results = await Promise.all(
+      handlers.map(async (handler): Promise<{ block: Block; receipts: Receipt[] } | null> => {
+        const header = await handler
+          .getBlockHeaders(height, new BN(1))
+          .then((headers) => (headers.length === 1 ? headers[0] : null))
+          .catch(() => null);
+        if (header === null) {
+          return null;
+        }
+
+        const body = await handler
+          .getBlockBodies([header])
+          .then((body) => (body.length === 1 ? body[0] : null))
+          .catch(() => null);
+        if (body === null) {
+          return null;
+        }
+
+        const block = Block.fromBlockData({ header, transactions: body }, { common: this.backend.getCommon(0), hardforkByBlockNumber: true });
+        try {
+          await preValidateBlock.call(block);
+        } catch (err) {
+          // ignore errors
+          return null;
+        }
+
+        // TODO: download and validate receipts
+        const receipts: Receipt[] = [];
+
+        return { block, receipts };
+      })
+    );
+
+    for (let i = 0; i < results.length; i++) {
+      const handler = handlers[i];
+      const result = results[i];
+      if (result === null) {
+        continue;
+      }
+
+      if (await this.updateConfirmContext(context, result.block, result.receipts, handler.peer.peerId)) {
+        break;
+      }
+    }
+  }
+
+  private async updateConfirmContext(context: ConfirmContext, block: Block, receipts: Receipt[], peerId: string) {
+    const { serializedHeader, serializedReceipts, confirmed, unconfirmed, height, td } = context;
+
+    let confirm = true;
+    if (!block.header.serialize().equals(serializedHeader)) {
+      confirm = false;
+    }
+    if (serializedReceipts.length !== receipts.length) {
+      confirm = false;
+    }
+    for (let i = 0; i > receipts.length; i++) {
+      if (!receipts[i].serialize().equals(serializedReceipts[i])) {
+        confirm = false;
+        break;
+      }
+    }
+
+    if (confirm) {
+      confirmed.add(peerId);
+    } else {
+      unconfirmed.add(peerId);
+    }
+
+    if (confirmed.size >= confirmLimit) {
+      // TODO: ban unconfirmed peers
+
+      await this.abortSync();
+
+      let snapSync: SnapSync | undefined;
+      let fullSync: any;
+      const localHeight = this.backend.getHeight();
+      if (height.sub(localHeight).gten(minSnapSyncInterval)) {
+        // use snap sync
+        snapSync = this.syncContext?.snapSync ?? new SnapSync('db' as any, 'network' as any); // TODO
+        await snapSync.setRoot(block.header.stateRoot);
+        snapSync.start();
+      } else {
+        // use full sync
+        fullSync = 'full' as any; // TODO
+      }
+
+      this.resetConfirmContext();
+      this.syncContext = {
+        td,
+        height,
+        block,
+        receipts,
+        start: localHeight,
+        snapSync,
+        fullSync
+      };
+
+      return true;
+    } else if (unconfirmed.size >= unconfirmLimit) {
+      // TODO: ban confirmed peers
+
+      this.resetConfirmContext();
+
+      return true;
+    } else {
+      // wait for enough confirm, do nothing
+      return false;
     }
   }
 

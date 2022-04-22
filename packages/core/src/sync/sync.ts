eb8040c6346e58da0ce5613eccdf44120c9a96ee
samlior <samlior@foxmail.com>
2022-04-22 11:14:53 +0800

    chore: improve sync logic

--- packages/core/src/sync/sync.ts
+++ packages/core/src/sync/sync.ts
@@ -43,6 +43,12 @@ type ConfirmContext = {
   timeout: NodeJS.Timeout;
 };
 
+enum ConfirmStatus {
+  WaitingForConfirm,
+  Confirmed,
+  Unconfirmed
+}
+
 export interface SyncBackend {
   getCommon(num: BNLike): Common;
   getHeight(): BN;
@@ -58,8 +64,8 @@ export class Sync {
   private isWorking: boolean = false;
   private timer = new AbortableTimer();
 
-  private syncContext?: SyncContext;
-  private confirmContext?: ConfirmContext;
+  syncContext?: SyncContext;
+  confirmContext?: ConfirmContext;
 
   private syncPromise?: Promise<void>;
   private randomPickPromise?: Promise<void>;
@@ -74,7 +80,7 @@ export class Sync {
     this.confirmContext = ctx;
   }
 
-  private async abortSync() {
+  private async abortSyncContext() {
     if (this.syncContext) {
       const { snapSync, fullSync } = this.syncContext;
       // TODO: abort full sync
@@ -94,39 +100,24 @@ export class Sync {
         const td = new BN(status.totalDifficulty);
 
         let block = ann.block;
-        const getRemoteBlock = async () => {
-          if (block) {
-            return block;
-          }
-
-          const [header] = await handler.getBlockHeaders(height, new BN(1));
-          const [body] = await handler.getBlockBodies([header]);
-          block = Block.fromBlockData({ header, transactions: body }, { common: this.backend.getCommon(0), hardforkByBlockNumber: true });
-
-          try {
-            await preValidateBlock.call(block);
-          } catch (err) {
-            // TODO: ban remote peer
-            throw err;
-          }
-
-          return block;
-        };
-
         let receipts: Receipt[] | undefined;
-        const getRemoteReceipts = async (): Promise<Receipt[]> => {
-          if (receipts) {
-            return receipts;
+        const downloadDataFromPeer = async () => {
+          if (block && receipts) {
+            return { block, receipts };
           }
 
-          // TODO: download receipt
-          // TODO: validate receipt
-          return [];
+          const result = await this.downloadDataFromPeer(height, handler);
+          if (result === null) {
+            throw new Error('download data from: ' + peerId + ' failed');
+          }
+
+          block = result.block;
+          receipts = result.receipts;
+          return { block, receipts };
         };
 
         const resetConfirmContextToCurrent = async () => {
-          const block = await getRemoteBlock();
-          const receipts = await getRemoteReceipts();
+          const { block, receipts } = await downloadDataFromPeer();
 
           const ctx: ConfirmContext = {
             td,
@@ -145,70 +136,18 @@ export class Sync {
           };
 
           this.resetConfirmContext(ctx);
+          await this.collectConfirm(ctx);
         };
 
         if (this.confirmContext) {
-          const { td: confirmTD, confirmed, unconfirmed, serializedHeader, serializedReceipts } = this.confirmContext;
+          const { td: confirmTD, confirmed, unconfirmed } = this.confirmContext;
           if (confirmTD.lte(this.backend.getTotalDifficulty())) {
             this.resetConfirmContext();
           } else if (td.gte(confirmTD) && !confirmed.has(peerId) && !unconfirmed.has(peerId)) {
-            const block = await getRemoteBlock();
-            const receipts = await getRemoteReceipts();
-
-            let confirm = true;
-            if (!block.header.serialize().equals(serializedHeader)) {
-              confirm = false;
-            }
-            if (serializedReceipts.length !== receipts.length) {
-              confirm = false;
-            }
-            for (let i = 0; i > receipts.length; i++) {
-              if (!receipts[i].serialize().equals(serializedReceipts[i])) {
-                confirm = false;
-                break;
-              }
-            }
-
-            if (confirm) {
-              confirmed.add(peerId);
-            } else {
-              unconfirmed.add(peerId);
-            }
-
-            if (confirmed.size >= confirmLimit) {
-              // TODO: ban unconfirmed peers
-
-              await this.abortSync();
-
-              let snapSync: SnapSync | undefined;
-              let fullSync: any;
-              const localHeight = this.backend.getHeight();
-              if (height.sub(localHeight).gten(minSnapSyncInterval)) {
-                // use snap sync
-                snapSync = this.syncContext?.snapSync ?? new SnapSync('db' as any, 'network' as any); // TODO
-                await snapSync.setRoot(block.header.stateRoot);
-                snapSync.start();
-              } else {
-                // use full sync
-                fullSync = 'full' as any; // TODO
-              }
-
-              this.resetConfirmContext();
-              this.syncContext = {
-                td: confirmTD,
-                height,
-                block,
-                receipts,
-                start: localHeight,
-                snapSync,
-                fullSync
-              };
-
+            const { block, receipts } = await downloadDataFromPeer();
+            const status = await this.updateConfirmContext(this.confirmContext, block, receipts, peerId);
+            if (status === ConfirmStatus.Confirmed || status === ConfirmStatus.WaitingForConfirm) {
               continue;
-            } else if (unconfirmed.size >= unconfirmLimit) {
-              // TODO: ban confirmed peers
-
-              this.resetConfirmContext();
             }
           }
         }
@@ -267,49 +206,16 @@ export class Sync {
 
   private async collectConfirm(context: ConfirmContext) {
     const { confirmed, unconfirmed, height } = context;
-
     const td = this.backend.getTotalDifficulty();
+
     const handlers = this.pool.handlers.filter(({ peer: { peerId }, status }) => {
       return new BN(status!.totalDifficulty).gt(td) && !confirmed.has(peerId) && !unconfirmed.has(peerId);
     });
-
     if (handlers.length === 0) {
       return;
     }
 
-    const results = await Promise.all(
-      handlers.map(async (handler): Promise<{ block: Block; receipts: Receipt[] } | null> => {
-        const header = await handler
-          .getBlockHeaders(height, new BN(1))
-          .then((headers) => (headers.length === 1 ? headers[0] : null))
-          .catch(() => null);
-        if (header === null) {
-          return null;
-        }
-
-        const body = await handler
-          .getBlockBodies([header])
-          .then((body) => (body.length === 1 ? body[0] : null))
-          .catch(() => null);
-        if (body === null) {
-          return null;
-        }
-
-        const block = Block.fromBlockData({ header, transactions: body }, { common: this.backend.getCommon(0), hardforkByBlockNumber: true });
-        try {
-          await preValidateBlock.call(block);
-        } catch (err) {
-          // ignore errors
-          return null;
-        }
-
-        // TODO: download and validate receipts
-        const receipts: Receipt[] = [];
-
-        return { block, receipts };
-      })
-    );
-
+    const results = await Promise.all(handlers.map(this.downloadDataFromPeer.bind(this, height)));
     for (let i = 0; i < results.length; i++) {
       const handler = handlers[i];
       const result = results[i];
@@ -317,13 +223,16 @@ export class Sync {
         continue;
       }
 
-      if (await this.updateConfirmContext(context, result.block, result.receipts, handler.peer.peerId)) {
+      const status = await this.updateConfirmContext(context, result.block, result.receipts, handler.peer.peerId);
+      if (status === ConfirmStatus.WaitingForConfirm) {
+        // do nothing
+      } else if (status === ConfirmStatus.Confirmed || status === ConfirmStatus.Unconfirmed) {
         break;
       }
     }
   }
 
-  private async updateConfirmContext(context: ConfirmContext, block: Block, receipts: Receipt[], peerId: string) {
+  private async updateConfirmContext(context: ConfirmContext, block: Block, receipts: Receipt[], peerId: string): Promise<ConfirmStatus> {
     const { serializedHeader, serializedReceipts, confirmed, unconfirmed, height, td } = context;
 
     let confirm = true;
@@ -349,7 +258,7 @@ export class Sync {
     if (confirmed.size >= confirmLimit) {
       // TODO: ban unconfirmed peers
 
-      await this.abortSync();
+      await this.abortSyncContext();
 
       let snapSync: SnapSync | undefined;
       let fullSync: any;
@@ -375,17 +284,46 @@ export class Sync {
         fullSync
       };
 
-      return true;
+      return ConfirmStatus.Confirmed;
     } else if (unconfirmed.size >= unconfirmLimit) {
       // TODO: ban confirmed peers
-
       this.resetConfirmContext();
-
-      return true;
+      return ConfirmStatus.Unconfirmed;
     } else {
       // wait for enough confirm, do nothing
-      return false;
+      return ConfirmStatus.WaitingForConfirm;
     }
+  }
+
+  private async downloadDataFromPeer(height: BN, handler: WireProtocolHandler): Promise<{ block: Block; receipts: Receipt[] } | null> {
+    const header = await handler
+      .getBlockHeaders(height, new BN(1))
+      .then((headers) => (headers.length === 1 ? headers[0] : null))
+      .catch(() => null);
+    if (header === null) {
+      return null;
+    }
+
+    const body = await handler
+      .getBlockBodies([header])
+      .then((body) => (body.length === 1 ? body[0] : null))
+      .catch(() => null);
+    if (body === null) {
+      return null;
+    }
+
+    const block = Block.fromBlockData({ header, transactions: body }, { common: this.backend.getCommon(0), hardforkByBlockNumber: true });
+    try {
+      await preValidateBlock.call(block);
+    } catch (err) {
+      // ignore errors
+      return null;
+    }
+
+    // TODO: download and validate receipts
+    const receipts: Receipt[] = [];
+
+    return { block, receipts };
   }
 
   announce(ann: Announcement) {
@@ -407,5 +345,7 @@ export class Sync {
     this.channel.abort();
     this.syncPromise && (await this.syncPromise);
     this.randomPickPromise && (await this.randomPickPromise);
+    this.resetConfirmContext();
+    await this.abortSyncContext();
   }
 }

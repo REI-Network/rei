7bc04e069519487bc069ce77dcbef50acb4eeabb
samlior <samlior@foxmail.com>
2022-06-27 13:53:09 +0800

    fix: database open options

--- packages/database/src/rawdb.ts
+++ packages/database/src/rawdb.ts
@@ -1,11 +1,11 @@
-import levelUp from 'levelup';
+import levelup from 'levelup';
 import encoding from 'encoding-down';
 import { leveldown } from '@rei-network/binding';
 
 export const createEncodingLevelDB = (path: string) => {
-  return levelUp(encoding(leveldown(path), { manifestFileMaxSize: 64 * 1024 * 1024 }));
+  return levelup(encoding(leveldown(path)), { manifestFileMaxSize: 64 * 1024 * 1024 });
 };
 
 export const createLevelDB = (path: string) => {
-  return levelUp(leveldown(path) as any, { manifestFileMaxSize: 64 * 1024 * 1024 });
+  return levelup(leveldown(path) as any, { manifestFileMaxSize: 64 * 1024 * 1024 });
 };

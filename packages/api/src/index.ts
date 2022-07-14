30fa50c5c1bd265b1679618778c6fcacc3cf1a55
bijianing97 <826015751@qq.com>
2022-07-14 16:21:51 +0800

    chore: move gasPriceOracle and filtersystem to package api

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,2 +1,4 @@
 export * from './apiServer';
 export * from './types';
+export * from './gasPriceOracle';
+export * from './filterSystem';
--- packages/api/src/types.ts
+++ packages/api/src/types.ts
@@ -2,6 +2,8 @@ import { Address, BN, bufferToHex } from 'ethereumjs-util';
 import { AbiCoder } from '@ethersproject/abi';
 import { hexStringToBuffer } from '@rei-network/utils';
 
+export type SyncingStatus = { syncing: true; status: { startingBlock: string; currentBlock: string; highestBlock: string } } | false;
+
 export type TopicsData = (string | null | (string | null)[])[];
 
 export type CallData = {
@@ -19,53 +21,15 @@ const coder = new AbiCoder();
 // keccak256("Error(string)").slice(0, 4)
 export const revertErrorSelector = Buffer.from('08c379a0', 'hex');
 
-const errors = {
-  PARSE_ERROR: {
-    code: -32700,
-    message: 'Parse error'
-  },
-  INVALID_REQUEST: {
-    code: -32600,
-    message: 'Invalid Request'
-  },
-  METHOD_NOT_FOUND: {
-    code: -32601,
-    message: 'Method not found'
-  },
-  INVALID_PARAMS: {
-    code: -32602,
-    message: 'Invalid params'
-  },
-  INTERNAL_ERROR: {
-    code: -32603,
-    message: 'Internal error'
-  },
-  SERVER_ERROR: {
-    code: -32000,
-    message: 'Server error'
-  },
-  REVERT_ERROR: {
-    code: 3
-  }
-};
-
 export class RevertError {
-  readonly code = errors.REVERT_ERROR.code;
-  readonly rpcMessage: string;
-  readonly data?: string;
+  readonly returnValue: Buffer | string;
 
   constructor(returnValue: Buffer | string) {
-    if (typeof returnValue === 'string') {
-      this.rpcMessage = returnValue;
-    } else {
-      this.rpcMessage = 'execution reverted: ' + coder.decode(['string'], returnValue.slice(4))[0];
-      this.data = bufferToHex(returnValue);
-    }
+    this.returnValue = returnValue;
   }
 }
 
 export class OutOfGasError {
-  readonly code = errors.SERVER_ERROR.code;
   readonly gas: BN;
 
   constructor(gas: BN) {
@@ -91,16 +55,12 @@ export function parseAddressesAndTopics(_addresses?: string | string[], _topics?
               return null;
             }
             if (typeof subTopic !== 'string') {
-              // TODO
-              //   helper.throwRpcErr('Invalid topic type');
+              throw new Error('Invalid topic type');
             }
             return hexStringToBuffer(subTopic);
           });
         } else {
-          // TODO
-          //   helper.throwRpcErr('Invalid topic type');
-          // for types.
-          return null;
+          throw new Error('Invalid topic type');
         }
       })
     : [];

e178d15994fe85f6a94078038e66ca9321ae6948
bijianing97 <826015751@qq.com>
2022-07-14 16:42:46 +0800

    chore: update rei controller

--- packages/rpc/src/controller/rei.ts
+++ packages/rpc/src/controller/rei.ts
@@ -1,8 +1,15 @@
-import { bnToHex, Address, intToHex } from 'ethereumjs-util';
-import { hexStringToBN } from '@rei-network/utils';
-import { Controller } from './base';
+import { ApiServer } from '@rei-network/api';
 
-export class ReiController extends Controller {
+/**
+ * Rei api Controller
+ */
+export class ReiController {
+  readonly apiServer: ApiServer;
+
+  constructor(apiServer: ApiServer) {
+    this.apiServer = apiServer;
+  }
+
   /**
    * Estimate user available crude
    * @param address - Target address
@@ -10,22 +17,7 @@ export class ReiController extends Controller {
    * @returns Available crude
    */
   async rei_getCrude([address, tag]: [string, string]) {
-    const block = await this.getBlockByTag(tag);
-    const common = block._common;
-    const strDailyFee = common.param('vm', 'dailyFee');
-    if (typeof strDailyFee !== 'string') {
-      return null;
-    }
-
-    const state = await this.backend.getStateManager(block.header.stateRoot, common);
-    const faddr = Address.fromString(common.param('vm', 'faddr'));
-    const totalAmount = (await state.getAccount(faddr)).balance;
-    const timestamp = block.header.timestamp.toNumber();
-    const dailyFee = hexStringToBN(strDailyFee);
-
-    const account = await state.getAccount(Address.fromString(address));
-    const stakeInfo = account.getStakeInfo();
-    return bnToHex(stakeInfo.estimateFee(timestamp, totalAmount, dailyFee));
+    return this.apiServer.getCrude(address, tag);
   }
 
   /**
@@ -35,12 +27,7 @@ export class ReiController extends Controller {
    * @returns Used crude
    */
   async rei_getUsedCrude([address, tag]: [string, string]) {
-    const block = await this.getBlockByTag(tag);
-    const timestamp = block.header.timestamp.toNumber();
-    const state = await this.backend.getStateManager(block.header.stateRoot, block._common);
-    const account = await state.getAccount(Address.fromString(address));
-    const stakeInfo = account.getStakeInfo();
-    return bnToHex(stakeInfo.estimateUsage(timestamp));
+    return this.apiServer.getUsedCrude(address, tag);
   }
 
   /**
@@ -50,10 +37,7 @@ export class ReiController extends Controller {
    * @returns Total deposit amount
    */
   async rei_getTotalAmount([address, tag]: [string, string]) {
-    const stateManager = await this.getStateManagerByTag(tag);
-    const account = await stateManager.getAccount(Address.fromString(address));
-    const stakeInfo = account.getStakeInfo();
-    return bnToHex(stakeInfo.total);
+    return this.apiServer.getTotalAmount(address, tag);
   }
 
   /**
@@ -62,13 +46,7 @@ export class ReiController extends Controller {
    * @returns Daily fee
    */
   async rei_getDailyFee([tag]: [string]) {
-    const num = await this.getBlockNumberByTag(tag);
-    const common = this.backend.getCommon(num);
-    const strDailyFee = common.param('vm', 'dailyFee');
-    if (typeof strDailyFee !== 'string') {
-      return null;
-    }
-    return bnToHex(hexStringToBN(strDailyFee));
+    return this.apiServer.getDailyFee(tag);
   }
 
   /**
@@ -77,12 +55,6 @@ export class ReiController extends Controller {
    * @returns Miner reward factor
    */
   async rei_getMinerRewardFactor([tag]: [string]) {
-    const num = await this.getBlockNumberByTag(tag);
-    const common = this.backend.getCommon(num);
-    const factor = common.param('vm', 'minerRewardFactor');
-    if (typeof factor !== 'number' || factor < 0 || factor > 100) {
-      return null;
-    }
-    return intToHex(factor);
+    return this.apiServer.getMinerRewardFactor(tag);
   }
 }

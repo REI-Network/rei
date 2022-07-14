827acd453f73f6b2daaf9ece862e376247a1c3ce
bijianing97 <826015751@qq.com>
2022-07-14 16:39:36 +0800

    chore: update web3 controller

--- packages/rpc/src/controller/web3.ts
+++ packages/rpc/src/controller/web3.ts
@@ -1,11 +1,29 @@
-import { bufferToHex, keccakFromHexString } from 'ethereumjs-util';
-import { Controller } from './base';
+import { ApiServer } from '@rei-network/api';
 
-export class Web3Controller extends Controller {
-  web3_clientVersion() {
-    return 'Mist/v0.0.1';
+/**
+ * Web3 api Controller
+ */
+export class Web3Controller {
+  readonly apiServer: ApiServer;
+
+  constructor(apiServer: ApiServer) {
+    this.apiServer = apiServer;
   }
+
+  /**
+   * Get client version
+   * @returns version data
+   */
+  web3_clientVersion() {
+    return this.apiServer.clientVersion();
+  }
+
+  /**
+   * Calulate the sha3 of a given string
+   * @param param0 - Data to calulate hash
+   * @returns Hash
+   */
   web_sha3([data]: [string]) {
-    return bufferToHex(keccakFromHexString(data));
+    return this.apiServer.sha3(data);
   }
 }

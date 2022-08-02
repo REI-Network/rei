4dc7b967c065436998f7995a9ce7f495745cdb08
samlior <samlior@foxmail.com>
2022-08-02 11:06:40 +0800

    chore: add migrate

---
+++ packages/database/src/migrate.ts
@@ -0,0 +1,36 @@
+import { LevelUp } from 'levelup';
+
+async function* asyncTraversal(db: LevelUp) {
+  const itr = db.iterator();
+  while (true) {
+    const result = await new Promise<[any, any] | void>((resolve, reject) => {
+      itr.next((err, key, val) => {
+        if (err) {
+          reject(err);
+        } else if (key === undefined || val === undefined) {
+          resolve();
+        } else {
+          resolve([key, val]);
+        }
+      });
+    });
+    if (!result) {
+      break;
+    }
+    yield result;
+  }
+  itr.end(() => {});
+}
+
+export async function migrate(from: LevelUp, to: LevelUp) {
+  console.log('migrate start');
+  try {
+    for await (const [key, val] of asyncTraversal(from)) {
+      await to.put(key, val);
+    }
+  } catch (err) {
+    console.log('migrate catch error:', err);
+  } finally {
+    console.log('migrate end');
+  }
+}

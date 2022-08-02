582cc6e0678702d4cc11619ad6586f142b811c5c
samlior <samlior@foxmail.com>
2022-08-02 10:54:00 +0800

    chore: add createRocksDB and createEncodingRocksDB

--- packages/database/src/rawdb.ts
+++ packages/database/src/rawdb.ts
@@ -1,5 +1,6 @@
 import levelup from 'levelup';
 import encoding from 'encoding-down';
+import rocksdown from 'rocksdb';
 import { leveldown } from '@rei-network/binding';
 
 export const createEncodingLevelDB = (path: string) => {
@@ -8,4 +9,12 @@ export const createEncodingLevelDB = (path: string) => {
 
 export const createLevelDB = (path: string) => {
   return levelup(leveldown(path) as any, { manifestFileMaxSize: 64 * 1024 * 1024 });
+};
+
+export const createEncodingRocksDB = (path: string) => {
+  return levelup(encoding(rocksdown(path)), { manifestFileMaxSize: 64 * 1024 * 1024 });
+};
+
+export const createRocksDB = (path: string) => {
+  return levelup(rocksdown(path), { manifestFileMaxSize: 64 * 1024 * 1024 });
 };

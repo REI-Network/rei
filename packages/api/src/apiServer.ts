1cf07e9631d89c773e0ac1bafdebdfb239e0ffd3
bijianing97 <826015751@qq.com>
2022-08-25 17:33:25 +0800

    chore: add admin functions

--- packages/api/src/apiServer.ts
+++ packages/api/src/apiServer.ts
@@ -991,19 +991,58 @@ export class ApiServer {
     return this.node.networkMngr.addPeer(enrTxt);
   }
 
-  //TODO
   /**
    * Disconnect remote peer
    * @param enrTxt - ENR string
    * @returns True if added sucessfully
    */
-  async removePeer(enrTxt: string) {}
+  async removePeer(enrTxt: string) {
+    return this.node.networkMngr.removeStaticPeer(enrTxt);
+  }
 
+  /**
+   * Add trusted peer,
+   * network manager will always accept connection from trusted peers,
+   * even if the number of connections is full
+   * @param enrTxt - ENR string
+   * @returns Whether the trusted node is added successfully
+   */
   async addTrustedPeer(enrTxt: string) {
     return this.node.networkMngr.addTrustedPeer(enrTxt);
   }
 
+  /**
+   * Remove trusted peer,
+   * NOTE: this method does not immediately modify peerValue
+   * @param enrTxt - ENR string
+   * @returns Whether the deletion of the trust node is successful
+   */
   async removeTrutedPeer(enrTxt: string) {
     return this.node.networkMngr.removeTrustedPeer(enrTxt);
+  }
+
+  /**
+   * Get connected peers
+   * @returns Peers information
+   */
+  async peers() {
+    return this.node.networkMngr.connectedPeers;
+  }
+
+  /**
+   * Get local node info
+   * @returns local node info
+   */
+  async nodeInfo() {
+    return this.node.networkMngr.nodeInfo;
+  }
+
+  /**
+   * Check remote peer is trusted
+   * @param enrTxt - ENR string
+   * @returns Whether it is a trusted node
+   */
+  async isTrusted(enrTxt: string) {
+    return this.node.networkMngr.isTrusted(enrTxt);
   }
 }
--- packages/ipc/controller/admin.ts
+++ packages/ipc/controller/admin.ts
@@ -40,4 +40,68 @@ export class AdminController {
       throw new Error('rpc server is not running');
     }
   }
+
+  /**
+   * Add static peer
+   * @param enrTxt - ENR string
+   * @returns True if added sucessfully
+   */
+  async addPeer([enrTxt]: [string]) {
+    return this.ipcServer.apiServer.addPeer(enrTxt);
+  }
+
+  /**
+   * Disconnect remote peer
+   * @param enrTxt - ENR string
+   * @returns True if added sucessfully
+   */
+  async removePeer([enrTxt]: [string]) {
+    return this.ipcServer.apiServer.removePeer(enrTxt);
+  }
+
+  /**
+   * Add trusted peer,
+   * network manager will always accept connection from trusted peers,
+   * even if the number of connections is full
+   * @param enrTxt - ENR string
+   * @returns Whether the trusted node is added successfully
+   */
+  async addTrustedPeer([enrTxt]: [string]) {
+    return this.ipcServer.apiServer.addTrustedPeer(enrTxt);
+  }
+
+  /**
+   * Remove trusted peer,
+   * NOTE: this method does not immediately modify peerValue
+   * @param enrTxt - ENR string
+   * @returns Whether the deletion of the trust node is successful
+   */
+  async removeTrutedPeer([enrTxt]: [string]) {
+    return this.ipcServer.apiServer.removeTrutedPeer(enrTxt);
+  }
+
+  /**
+   * Get connected peers
+   * @returns Peers information
+   */
+  async peers() {
+    return this.ipcServer.apiServer.peers();
+  }
+
+  /**
+   * Get local node info
+   * @returns local node info
+   */
+  async nodeInfo() {
+    return this.ipcServer.apiServer.nodeInfo();
+  }
+
+  /**
+   * Check remote peer is trusted
+   * @param enrTxt - ENR string
+   * @returns Whether it is a trusted node
+   */
+  async isTrusted([enrTxt]: [string]) {
+    return this.ipcServer.apiServer.isTrusted(enrTxt);
+  }
 }

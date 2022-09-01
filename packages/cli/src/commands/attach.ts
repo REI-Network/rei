29ac6500f0b72dcfe76c5b2541141166dd11a010
samlior <samlior@foxmail.com>
2022-09-01 15:43:27 +0800

    chore: improve attach and console command

--- packages/cli/src/commands/attach.ts
+++ packages/cli/src/commands/attach.ts
@@ -1,19 +1,20 @@
 import path from 'path';
-import { Command } from 'commander';
 import { logger } from '@rei-network/utils';
 import { IpcClient } from '@rei-network/ipc';
 
 export function installAttachCommand(program: any) {
-  const attach = new Command('attach [ipcpath]').description('Start an interactive JavaScript environment (connect to node)').action((ipcPath) => {
-    try {
-      const client = new IpcClient({
-        datadir: program.opts().datadir,
-        ipcPath: ipcPath ? path.resolve(ipcPath) : undefined
-      });
-      client.start();
-    } catch (err) {
-      logger.error('IPC, attach, error:', err);
-    }
-  });
-  program.addCommand(attach);
+  program
+    .command('attach [ipcpath]')
+    .description('Start an interactive JavaScript environment (connect to node)')
+    .action((ipcPath) => {
+      try {
+        const client = new IpcClient({
+          datadir: program.opts().datadir,
+          ipcPath: ipcPath ? path.resolve(ipcPath) : undefined
+        });
+        client.start();
+      } catch (err) {
+        logger.error('IPC, attach, error:', err);
+      }
+    });
 }
--- packages/cli/src/commands/console.ts
+++ packages/cli/src/commands/console.ts
@@ -1,19 +1,20 @@
-import { Command } from 'commander';
 import { logger } from '@rei-network/utils';
 import { IpcClient } from '@rei-network/ipc';
 import { startNode } from './start';
 
 export function installConsoleCommand(program: any) {
-  const console = new Command('console').description('Start an interactive JavaScript environment').action(async () => {
-    try {
-      const opts = program.opts();
-      await startNode(opts);
-      const client = new IpcClient({ datadir: opts.datadir });
-      client.start();
-    } catch (err) {
-      logger.error('Start error:', err);
-      process.exit(1);
-    }
-  });
-  program.addCommand(console);
+  program
+    .command('console')
+    .description('Start an interactive JavaScript environment')
+    .action(async () => {
+      try {
+        const opts = program.opts();
+        await startNode(opts);
+        const client = new IpcClient({ datadir: opts.datadir });
+        client.start();
+      } catch (err) {
+        logger.error('Start error:', err);
+        process.exit(1);
+      }
+    });
 }

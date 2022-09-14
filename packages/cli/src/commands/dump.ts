27bee7675241f2df93c57b17bec48175cc009f45
maqicheng <1504848701@qq.com>
2022-09-14 18:06:31 +0800

    fix :fix dump

--- packages/cli/src/commands/dump.ts
+++ packages/cli/src/commands/dump.ts
@@ -4,18 +4,18 @@ import { logger } from '@rei-network/utils';
 
 export async function installDumpCommand(program: any) {
   program
-    .command('dump')
-    .description('Dump rocksdb database')
+    .command('dump-leveldb')
+    .description('Dump leveldb database')
     .action(() => {
       const { datadir } = program.opts();
       try {
-        logger.info('Dump rocksdb database start');
-        dump(path.join(datadir, 'chaindb-rocks'));
-        dump(path.join(datadir, 'nodes-rocks'));
-        dump(path.join(datadir, 'evidence-rocks'));
-        logger.info('Dump rocksdb database done');
+        logger.info('Dump leveldb database start');
+        dump(path.join(datadir, 'chaindb'));
+        dump(path.join(datadir, 'nodes'));
+        dump(path.join(datadir, 'evidence'));
+        logger.info('Dump leveldb database done');
       } catch (err) {
-        logger.error('Dump rocksdb database:', err);
+        logger.error('Dump leveldb database:', err);
         process.exit(1);
       }
     });

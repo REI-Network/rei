5f3e95a89a59751f4f3419f8f48063e2a62b9571
bijianing97 <826015751@qq.com>
2022-09-22 18:17:46 +0800

    chore: init jail test

---
+++ packages/contracts/test/jail.test.ts
@@ -0,0 +1,31 @@
+import type Web3 from 'web3';
+import { assert, expect } from 'chai';
+import { BN, MAX_INTEGER } from 'ethereumjs-util';
+import { upTimestamp, toBN } from './utils';
+
+declare var artifacts: any;
+declare var web3: Web3;
+
+const Config = artifacts.require('Config_devnet');
+const Jail = artifacts.require('Jail');
+
+describe('Jail', () => {
+  let config: any;
+  let jail: any;
+  let deployer: any;
+  let user1: any;
+
+  before(async () => {
+    const accounts = await web3.eth.getAccounts();
+    deployer = accounts[0];
+    user1 = accounts[1];
+  });
+
+  it('should deploy succeed', async () => {
+    config = new web3.eth.Contract(Config.abi, (await Config.new()).address, { from: deployer });
+    await config.methods.setSystemCaller(deployer).send();
+
+    jail = new web3.eth.Contract(Jail.abi, (await Jail.new(config.options.address)).address, { from: deployer });
+    await config.methods.setJail(jail.options.address).send();
+  });
+});

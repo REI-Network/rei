b5c594ebf3975aaddd98aa0896fae6df2908a6c2
samlior <samlior@foxmail.com>
2022-11-17 17:23:54 +0800

    chore: remove tasks

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -2,7 +2,6 @@ import '@typechain/hardhat';
 import '@nomiclabs/hardhat-etherscan';
 import '@nomiclabs/hardhat-truffle5';
 import 'hardhat-deploy';
-import './tasks';
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more

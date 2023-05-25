bd525fa7b6c5d886ec3f587beb702456d951427f
maqicheng <1504848701@qq.com>
2023-05-25 15:07:10 +0800

    chore : add notes

--- packages/core/src/consensus/reimint/contracts/storageLoader.ts
+++ packages/core/src/consensus/reimint/contracts/storageLoader.ts
@@ -2,34 +2,67 @@ import { Address, BN, keccak256, setLengthLeft } from 'ethereumjs-util';
 import { StateManager } from '../../../stateManager/stateManager';
 
 export class StorageLoader {
-  private readonly stateManager: StateManager;
-  private readonly address: Address;
+  private readonly stateManager: StateManager; // The state manager
+  private readonly address: Address; // The address of the contract
 
   constructor(stateManager: StateManager, address: Address) {
     this.stateManager = stateManager;
     this.address = address;
   }
 
+  /**
+   * Convert an index to a slot index
+   * @param index The index to convert
+   * @returns The slot index
+   */
   static indexToSlotIndex(index: BN): Buffer {
     return setLengthLeft(index.toBuffer(), 32);
   }
 
+  /**
+   * get the storage index for a mapping
+   * @param slotIndex The slot index of the mapping
+   * @param key The key of the mapping
+   * @returns The storage index
+   */
   public getMappingStorageIndex(slotIndex: Buffer, key: Buffer) {
     return keccak256(Buffer.concat([setLengthLeft(key, 32), slotIndex]));
   }
 
+  /**
+   * get the storage index for an array
+   * @param slotIndex The slot index of the array
+   * @param index The index of the array
+   * @returns The storage index
+   */
   public getArrayStorageIndex(slotIndex: Buffer, index: BN) {
     return setLengthLeft(new BN(keccak256(slotIndex)).add(index).toBuffer(), 32);
   }
 
+  /**
+   * get the storage index for a struct
+   * @param slotIndex The slot index of the struct
+   * @param index The index of the struct
+   * @returns The storage index
+   */
   public getStructStorageIndex(slotIndex: Buffer, index: BN) {
     return setLengthLeft(new BN(slotIndex).add(index).toBuffer(), 32);
   }
 
+  /**
+   * Load the storage slot
+   * @param slotIndex The slot index to load
+   * @returns The storage slot
+   */
   async loadStorageSlot(slotIndex: Buffer): Promise<Buffer> {
     return await this.stateManager.getContractStorage(this.address, slotIndex);
   }
 
+  /**
+   * Load the storage slot as a string or bytes
+   * @param slotIndex The slot index to load
+   * @returns The storage slot as a string
+   */
   async loadBytesOrString(slotIndex: Buffer): Promise<Buffer> {
     const storageSlot = await this.stateManager.getContractStorage(this.address, slotIndex);
     if (storageSlot[31] % 2 === 0) {

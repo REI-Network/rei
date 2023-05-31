b7e8bd1ce684edc940898857d846ed4677c29082
maqicheng <1504848701@qq.com>
2023-05-31 16:48:23 +0800

    fix : fix loadBytesOrString function

--- packages/core/src/consensus/reimint/contracts/storageLoader.ts
+++ packages/core/src/consensus/reimint/contracts/storageLoader.ts
@@ -33,10 +33,11 @@ export class StorageLoader {
    * get the storage index for an array
    * @param slotIndex The slot index of the array
    * @param index The index of the array
+   * @param step The step of the element, it can be understood as how many storage slots the element occupies
    * @returns The storage index
    */
-  public getArrayStorageIndex(slotIndex: Buffer, index: BN) {
-    return setLengthLeft(new BN(keccak256(slotIndex)).add(index).toBuffer(), 32);
+  public getArrayStorageIndex(slotIndex: Buffer, index: BN, step: BN = new BN(1)) {
+    return setLengthLeft(new BN(keccak256(slotIndex)).add(index.mul(step)).toBuffer(), 32);
   }
 
   /**
@@ -64,9 +65,14 @@ export class StorageLoader {
    * @returns The storage slot as a string
    */
   async loadBytesOrString(slotIndex: Buffer): Promise<Buffer> {
-    const storageSlot = await this.stateManager.getContractStorage(this.address, slotIndex);
-    if (storageSlot[31] % 2 === 0) {
-      return storageSlot.slice(0, storageSlot[31] / 2);
+    let storageSlot = await this.stateManager.getContractStorage(this.address, slotIndex);
+    if (storageSlot.length === 0) {
+      return storageSlot;
+    }
+    const last = storageSlot[storageSlot.length - 1];
+    if (last % 2 === 0) {
+      storageSlot = setLengthLeft(storageSlot, 32);
+      return storageSlot.slice(0, last / 2);
     } else {
       const tempBufferArray = new Array<Buffer>();
       const len = new BN(storageSlot).subn(1).divn(2);
@@ -74,7 +80,7 @@ export class StorageLoader {
       const slotLen = tempMod > 0 ? len.divn(32).addn(1) : len.divn(32);
       const slotHash = keccak256(slotIndex);
       for (let i = new BN(0); i.lt(slotLen); i = i.addn(1)) {
-        let tempSlot = await this.stateManager.getContractStorage(this.address, new BN(slotHash).iadd(i).toBuffer());
+        let tempSlot = setLengthLeft(await this.stateManager.getContractStorage(this.address, new BN(slotHash).iadd(i).toBuffer()), 32);
         if (i.eq(slotLen.subn(1))) {
           tempSlot = tempSlot.slice(0, tempMod);
         }
